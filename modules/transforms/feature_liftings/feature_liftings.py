5918c26444ccded430272fabaebb3afe9a029571
Martin Carrasco <martin.carrasco@utec.edu.pe>
2024-05-18 19:49:29 +0200

    ADD: Mean projection for feature lifting

--- modules/transforms/feature_liftings/feature_liftings.py
+++ modules/transforms/feature_liftings/feature_liftings.py
@@ -54,3 +54,56 @@ class ProjectionSum(torch_geometric.transforms.BaseTransform):
             The lifted data.
         """
         return self.lift_features(data)
+
+class ProjectionMean(torch_geometric.transforms.BaseTransform):
+    r"""Lifts r-cell features to r+1-cells by projection using the mean
+
+    Parameters
+    ----------
+    **kwargs : optional
+        Additional arguments for the class.
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__()
+
+    def lift_features(
+        self, data: torch_geometric.data.Data | dict
+    ) -> torch_geometric.data.Data | dict:
+        r"""Projects r-cell features of a graph to r+1-cell structures using the incidence matrix.
+
+        Parameters
+        ----------
+        data : torch_geometric.data.Data | dict
+            The input data to be lifted.
+
+        Returns
+        -------
+        torch_geometric.data.Data | dict
+            The lifted data."""
+        keys = sorted([key.split("_")[1] for key in data.keys() if "incidence" in key])  # noqa : SIM118
+        for elem in keys:
+            if f"x_{elem}" not in data:
+                idx_to_project = 0 if elem == "hyperedges" else int(elem) - 1
+                data["x_" + elem] = torch.matmul(
+                    abs(data["incidence_" + elem].t()),
+                    data[f"x_{idx_to_project}"],
+                ).mean(dim=1) #TODO check if this is correct
+        return data
+
+    def forward(
+        self, data: torch_geometric.data.Data | dict
+    ) -> torch_geometric.data.Data | dict:
+        r"""Applies the lifting to the input data.
+
+        Parameters
+        ----------
+        data : torch_geometric.data.Data | dict
+            The input data to be lifted.
+
+        Returns
+        -------
+        torch_geometric.data.Data | dict
+            The lifted data.
+        """
+        return self.lift_features(data)
