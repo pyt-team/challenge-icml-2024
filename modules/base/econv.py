3ba714a7709d8e121c86c1ddd1890507cedd6cad
Martin Carrasco <martin.carrasco@utec.edu.pe>
2024-05-20 18:50:13 +0200

    FIX: Econv to reflect the actual convolution

--- modules/base/econv.py
+++ modules/base/econv.py
@@ -1,27 +1,30 @@
 """Convolutional layer for message passing."""
+import math
 from typing import Literal
 
 import torch
 from torch.nn.parameter import Parameter
+from torch_scatter import scatter_add
 
-from topomodelx.base.message_passing import MessagePassing
+from topomodelx.base.conv import Conv
 
 
-class EConv(MessagePassing):
+class EConv(Conv):
     """Message passing: steps 1, 2, and 3.
 
-    Builds the message passing route given by one neighborhood matrix.
-    Includes an option for an x-specific update function.
-
+    Builds on the Conv class to implement message passing but 
+    adds MLPs to different sections of the convolution procedure
     Parameters
     ----------
     in_channels : int
         Dimension of input features.
+    weight_channels : int
+        Dimension of the edge weights.
     out_channels : int
         Dimension of output features.
     aggr_norm : bool, default=False
         Whether to normalize the aggregated message by the neighborhood size.
-    update_func : {"relu", "sigmoid"}, optional
+    update_func : {"relu", "sigmoid", "silu"}, optional
         Update method to apply to message.
     att : bool, default=False
         Whether to use attention.
@@ -29,42 +32,64 @@ class EConv(MessagePassing):
         Initialization method.
     initialization_gain : float, default=1.414
         Initialization gain.
-    with_linear_transform : bool, default=True
-        Whether to apply a learnable linear transform.
+    with_linear_transform_1 : bool, default=True
+        Whether to apply a first learnable linear transform.
         NB: if `False` in_channels has to be equal to out_channels.
+    with_linear_transform_2 : bool, default=True
+        Whether to apply a second learnable linear transform.
+    with_weighted_message : bool, default=True
+        Whether to apply a learnable message weighting
     """
 
     def __init__(
         self,
         in_channels,
+        weight_channels,
         out_channels,
         aggr_norm: bool = False,
         update_func: Literal["relu", "sigmoid", "silu", None] = None,
         att: bool = False,
         initialization: Literal["xavier_uniform", "xavier_normal"] = "xavier_uniform",
         initialization_gain: float = 1.414,
-        with_linear_transform: bool = True,
+        with_linear_transform_1: bool = True,
+        with_linear_transform_2: bool = True,
+        with_weighted_message: bool = True
     ) -> None:
         super().__init__(
             att=att,
             initialization=initialization,
             initialization_gain=initialization_gain,
         )
+        self.weight_channels = weight_channels
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.aggr_norm = aggr_norm
         self.update_func = update_func
 
-        self.weight = (
-            Parameter(torch.Tensor(self.in_channels, self.out_channels))
-            if with_linear_transform
+        self.weight_1 = (
+            Parameter(torch.Tensor(2*self.in_channels + weight_channels, self.in_channels))
+            if with_linear_transform_1
+            else None
+        )
+        self.weight_2 = (
+            Parameter(torch.Tensor(self.in_channels, self.in_channels))
+            if with_linear_transform_2
             else None
         )
 
-        if not with_linear_transform and in_channels != out_channels:
+        self.weight_3 = (
+            Parameter(torch.Tensor(self.in_channels, self.out_channels))
+            if with_weighted_message
+            else None
+        )
+
+        # TODO correct different cases here
+        if not with_linear_transform_1 and not with_linear_transform_2 and not with_weighted_message and in_channels != out_channels:
             raise ValueError(
                 "With `linear_trainsform=False`, in_channels has to be equal to out_channels"
             )
+
+        # TODO correct attention possibility
         if self.att:
             self.att_weight = Parameter(
                 torch.Tensor(
@@ -73,6 +98,61 @@ class EConv(MessagePassing):
             )
 
         self.reset_parameters()
+
+    def reset_parameters(self):
+        match self.initialization:
+            case "uniform":
+                if self.weight_1 is not None:
+                    stdv = 1.0 / math.sqrt(self.weight.size(1))
+                    self.weight_1.data.uniform_(-stdv, stdv)
+                if self.weight_2 is not None:
+                    stdv = 1.0 / math.sqrt(self.weight.size(1))
+                    self.weight_1.data.uniform_(-stdv, stdv)
+                if self.weight_2 is not None:
+                    stdv = 1.0 / math.sqrt(self.weight.size(1))
+                    self.weight_1.data.uniform_(-stdv, stdv)
+                if self.att:
+                    stdv = 1.0 / math.sqrt(self.att_weight.size(1))
+                    self.att_weight.data.uniform_(-stdv, stdv)
+            case "xavier_uniform":
+                if self.weight_1 is not None:
+                    torch.nn.init.xavier_uniform_(
+                        self.weight, gain=self.initialization_gain
+                    )
+                if self.weight_2 is not None:
+                    torch.nn.init.xavier_uniform_(
+                        self.weight, gain=self.initialization_gain
+                    )
+                if self.weight_3 is not None:
+                    torch.nn.init.xavier_uniform_(
+                        self.weight, gain=self.initialization_gain
+                    )
+                if self.att:
+                    torch.nn.init.xavier_uniform_(
+                        self.att_weight.view(-1, 1), gain=self.initialization_gain
+                    )
+            case "xavier_normal":
+                if self.weight_1 is not None:
+                    torch.nn.init.xavier_normal_(
+                        self.weight, gain=self.initialization_gain
+                    )
+                if self.weight_2 is not None:
+                    torch.nn.init.xavier_normal_(
+                        self.weight, gain=self.initialization_gain
+                    )
+                if self.weight_3 is not None:
+                    torch.nn.init.xavier_normal_(
+                        self.weight, gain=self.initialization_gain
+                    )
+                if self.att:
+                    torch.nn.init.xavier_normal_(
+                        self.att_weight.view(-1, 1), gain=self.initialization_gain
+                    )
+            case _:
+                raise ValueError(
+                    f"Initialization {self.initialization} not recognized."
+                )
+
 
     def update(self, x_message_on_target) -> torch.Tensor:
         """Update embeddings on each cell (step 4).
@@ -95,13 +175,14 @@ class EConv(MessagePassing):
             return torch.nn.functional.silu(x_message_on_target)
         return x_message_on_target
 
-    def forward(self, x_source, neighborhood, x_target=None) -> torch.Tensor:
+    def forward(self, x_source, edge_index, x_weights, x_target=None) -> torch.Tensor:
         """Forward pass.
 
         This implements message passing:
         - from source cells with input features `x_source`,
         - via `neighborhood` defining where messages can pass,
         - to target cells with input features `x_target`.
+        - using a certain set of weights `x_weights`
 
         In practice, this will update the features on the target cells.
 
@@ -113,8 +194,10 @@ class EConv(MessagePassing):
         x_source : Tensor, shape = (..., n_source_cells, in_channels)
             Input features on source cells.
             Assumes that all source cells have the same rank r.
-        neighborhood : torch.sparse, shape = (n_target_cells, n_source_cells)
-            Neighborhood matrix.
+        x_weights : Tensor, shape = (..., n_edges, weight_channels)
+            Weights of the relation connecting `x_source` and `x_target`.
+        neighborhood : torch.sparse, shape = (2, n_edges)
+            Edge index format for neighborhood.
         x_target : Tensor, shape = (..., n_target_cells, in_channels)
             Input features on target cells.
             Assumes that all target cells have the same rank s.
@@ -127,6 +210,9 @@ class EConv(MessagePassing):
             Output features on target cells.
             Assumes that all target cells have the same rank s.
         """
+
+        # TODO Fix attention
+        '''
         if self.att:
             neighborhood = neighborhood.coalesce()
             self.target_index_i, self.source_index_j = neighborhood.indices()
@@ -136,16 +222,34 @@ class EConv(MessagePassing):
                 values=attention_values * neighborhood.values(),
                 size=neighborhood.shape,
             )
-        if self.weight is not None:
-            x_message = torch.mm(x_source, self.weight)
-        else:
-            x_message = x_source
-        x_message_on_target = torch.mm(neighborhood, x_message)
+        '''
+        # Construct the edge index tensor of size (2, n_boundaries)
+        send_idx, recv_idx = edge_index
 
+        x_message = torch.cat((x_source[send_idx], x_target[recv_idx], x_weights), dim=1) 
+
+        if self.weight_1 is not None:
+            x_message = torch.mm(x_message, self.weight_1)
+            x_message = self.update(x_message)
+        if self.weight_2 is not None:
+            x_message = torch.mm(x_message, self.weight_2)
+            x_message = self.update(x_message)
+        if self.weight_3 is not None:
+            x_message_weights =  torch.mm(x_message, self.weight_3)
+            self.x_message_weights = torch.nn.Sigmoid(x_message_weights)
+        else:
+            x_message_weights = torch.ones_like(x_message)
+
+        # Weight the message by the learned weights
+        x_message = x_message * x_message_weights
+
+        # TODO correct aggregation
+        '''
         if self.aggr_norm:
             neighborhood_size = torch.sum(neighborhood.to_dense(), dim=1)
             x_message_on_target = torch.einsum(
                 "i,ij->ij", 1 / neighborhood_size, x_message_on_target
             )
+        '''
 
-        return self.update(x_message_on_target)
+        return scatter_add(x_message, recv_idx, dim=0, dim_size=x_target.size(0))
