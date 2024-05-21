dff89996a01106f53c9ae88c6e46d508171fde1d
Martin Carrasco <martin.carrasco@utec.edu.pe>
2024-05-21 16:42:13 +0200

    FIX: Ranking, batching, pooling and naming for ditionary

--- modules/models/simplicial/empsn.py
+++ modules/models/simplicial/empsn.py
@@ -18,13 +18,13 @@ class EMPSN(nn.Module):
 
         # Create `num_layers` layers with sum aggregation and SiLU update function
         tmp_dict = {
-            0: {
-                0: 3,
-                1: 3
+            "rank_0": {
+                f"rank_0": 3,
+                f"rank_1": 3
             },
-            1: {
-                1: 6,
-                2: 6
+            "rank_1": {
+                f"rank_1": 3,
+                f"rank_2": 3
             }
         }
 
@@ -35,13 +35,13 @@ class EMPSN(nn.Module):
         # Pre-pooling operation
         self.pre_pool = nn.ModuleDict(
             {
-            str(dim): 
+                f"rank_{rank}": 
                 nn.Sequential(
                     nn.Linear(hidden_channels, hidden_channels),
                     nn.SiLU(),
                     nn.Linear(hidden_channels, hidden_channels)
                 )
-                for dim in range(self.max_dim+1)
+                for rank in range(self.max_dim+1)
             }
         )
 
@@ -56,7 +56,7 @@ class EMPSN(nn.Module):
 
     def forward(self, features: Dict[int, Tensor], edge_index_adjacencies: Dict[int, Tensor],
                 edge_index_incidences: Dict[int, Tensor], invariances_r_r: Dict[int, Tensor],
-                invariances_r_r_minus_1: Dict[int, Tensor]) -> Tensor:
+                invariances_r_r_minus_1: Dict[int, Tensor], x_batch: Dict[int, Tensor]) -> Tensor:
         r"""Forward pass.
 
         Parameters
@@ -71,6 +71,8 @@ class EMPSN(nn.Module):
             Adjacency matrices :math:`I^0_r` with weights of cells to cells via lower and upper cells.
         invariances_r_r_minus_1 : dict[int, torch.sparse], length=max_rank, shape = (n_rank_r_minus_1_cells, n_rank_r_cells)
             Adjacency matrices :math:`I^1_r` with weights of map from r-cells to (r-1)-cells
+        x_batch : dict[int, torch.Tensor], length=max_rank+1, shape = (n_rank_r_cells, channels)
+            Batching information of the features  
 
         Returns
         -------
@@ -78,15 +80,18 @@ class EMPSN(nn.Module):
             Regression value of the pooling of the graph
         """
 
-        x = features 
+        x = {
+            rank: self.feature_embedding(feature) 
+            for rank, feature in features.items()
+        }
         for layer in self.layers:
             x = layer(x, edge_index_adjacencies, edge_index_incidences, invariances_r_r, invariances_r_r_minus_1)
 
         # read out
-        x = {dim: self.pre_pool[dim](feature) for dim, feature in x.items()}
+        x = {rank: self.pre_pool[rank](feature) for rank, feature in x.items()}
         # TODO how to batch
-        x = {dim: global_add_pool(x[dim]) for dim, feature in x.items()}
-        state = torch.cat(tuple([feature for dim, feature in x.items()]), dim=1)
+        x = {rank: global_add_pool(x[rank], batch=x_batch[rank]) for rank, feature in x.items()}
+        state = torch.cat(tuple([feature for rank, feature in x.items()]), dim=1)
         out = self.post_pool(state) # Classifier across 19 variables 
         out = torch.squeeze(out)
 
