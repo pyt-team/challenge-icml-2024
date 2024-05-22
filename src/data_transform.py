14cbef3b5b28b2725f61f30303ed644ea6a924a9
Martin Carrasco <martin.carrasco@utec.edu.pe>
2024-05-23 00:52:58 +0200

    ADD: Alpha transform

--- src/data_transform.py
+++ src/data_transform.py
@@ -3,11 +3,21 @@ import torch
 from torch_geometric.data import Data
 
 from modules.transforms.liftings.graph2simplicial.vietoris_rips_lift import SimplicialVietorisRipsLifting
+from modules.transforms.liftings.pointcloud2simplicial.alpha_complex_lift import SimplicialAlphaComplexLifting
 
 qm9_to_ev = {'U0': 27.2114, 'U': 27.2114, 'G': 27.2114, 'H': 27.2114, 'zpve': 27211.4, 'gap': 27.2114, 'homo': 27.2114, 'lumo': 27.2114}
 targets = ['mu', 'alpha', 'homo', 'lumo', 'gap', 'r2', 'zpve', 'U0', 'U', 'H', 'G', 'Cv', 'U0_atom', 'U_atom', 'H_atom', 'G_atom', 'A', 'B', 'C']
 
-class SimplicialPreTransfrom(SimplicialVietorisRipsLifting):
+class AlphaPreTransform(SimplicialAlphaComplexLifting):
+    def __init__(self, **kwargs):
+        self.target_name = kwargs['target_name']
+        super().__init__(**kwargs)
+
+    def __call__(self, data: Data) -> Data:
+        data = prepare_data(data, self.target_name, qm9_to_ev)
+        data = super().__call__(data)
+        return data
+class VietorisRipsPreTransfrom(SimplicialVietorisRipsLifting):
     def __init__(self, dis: float = 0, **kwargs):
         self.target_name = kwargs['target_name']
         super().__init__(dis=dis, **kwargs)
