6b93d8ae8984a5e25127426b5f95a130814c77cf
Martin Carrasco <martin.carrasco@utec.edu.pe>
2024-05-23 21:50:16 +0200

    ADD: Filtering function for AlphaComplex

--- src/data_transform.py
+++ src/data_transform.py
@@ -1,9 +1,12 @@
 from typing import Dict
 import torch
 from torch_geometric.data import Data
+import gudhi
+from gudhi import SimplexTree
 
 from modules.transforms.liftings.graph2simplicial.vietoris_rips_lift import SimplicialVietorisRipsLifting
-from modules.transforms.liftings.pointcloud2simplicial.alpha_complex_lift import SimplicialAlphaComplexLifting
+from modules.transforms.liftings.graph2simplicial.alpha_complex_lift import SimplicialAlphaComplexLifting
+from toponetx.classes import SimplicialComplex
 
 qm9_to_ev = {'U0': 27.2114, 'U': 27.2114, 'G': 27.2114, 'H': 27.2114, 'zpve': 27211.4, 'gap': 27.2114, 'homo': 27.2114, 'lumo': 27.2114}
 targets = ['mu', 'alpha', 'homo', 'lumo', 'gap', 'r2', 'zpve', 'U0', 'U', 'H', 'G', 'Cv', 'U0_atom', 'U_atom', 'H_atom', 'G_atom', 'A', 'B', 'C']
@@ -26,6 +29,22 @@ class VietorisRipsPreTransfrom(SimplicialVietorisRipsLifting):
         data = prepare_data(data, self.target_name, qm9_to_ev)
         data = super().__call__(data)
         return data
+
+def filter_not_enough_simplices_alpha(graph: Data):
+
+    pos = graph.pos
+
+    # Create a list of each node tensor position 
+    points = [pos[i].tolist() for i in range(pos.shape[0])]
+
+    # Lift the graph to an AlphaComplex
+    alpha_complex = gudhi.AlphaComplex(points=points)
+    simplex_tree: SimplexTree = alpha_complex.create_simplex_tree(default_filtration_value=True)
+    simplex_tree.prune_above_dimension(2)
+    simplicial_complex = SimplicialComplex.from_gudhi(simplex_tree)
+    return simplicial_complex.maxdim > 1
+    
+
 
 def prepare_data(graph: Data, target_name: str, qm9_to_ev: Dict[str, float]) -> Data:
     index = targets.index(target_name)
