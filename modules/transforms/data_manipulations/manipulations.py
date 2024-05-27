FIX: Faster calculation for invariances

modules/transforms/data_manipulations/manipulations.py
@@ -353,64 +353,44 @@ def compute_invariance_r_to_r(simplices: Dict[int, torch.Tensor], pos: torch.Ten
     """
 
     adj_dict={}
-    # for 0 dimensional simplices:
-    result_matrix_0 = []
-    #loop over number of simplexes (we use the iterator as an simplex_id)
-    for sending_simplex_id in range(adj[0].shape[0]):
-        if sum(adj[0][sending_simplex_id]) == 0:
-            continue
-        row = adj[0][sending_simplex_id]
-        #get the indexes all connected simplices in the adjecency matrix.
-        connected_simplex_ids = [idx for idx, v in enumerate(row) if idx != sending_simplex_id and v != 0]
-        # get connected nodes of connected simplices (redundant for dim 0)
-        connected_nodes = simplices[0][connected_simplex_ids]
-        # create tensor of sending node for efficient feature calculation
-        node_tensor = torch.full((len(connected_nodes),1), sending_simplex_id)
-        distance = torch.linalg.norm(pos[node_tensor] - pos[connected_nodes], dim=2)
-        # two because area_send, area_receive
-        area = torch.zeros(len(connected_nodes), 2)
-        result_matrix_0.append(torch.cat((distance, area), dim=1))
-    adj_dict[0] = torch.cat(result_matrix_0, dim=0)
+    sending_nodes = adj[0][0]
+    receiving_nodes = adj[0][1]
+    distance = torch.linalg.norm(pos[sending_nodes] - pos[receiving_nodes], dim=1)
+    area = torch.zeros(len(distance), 2)
+    adj_dict[0] = torch.cat((distance.unsqueeze(1), area),dim=1)
+    
     #for 1 dimensional simplexes
-    result_matrix_1 = []
-    for sending_simplex_id in range(adj[1].shape[0]):
-        if sum(adj[1][sending_simplex_id]) == 0:
-            continue
-        row = adj[1][sending_simplex_id]
-        connected_simplex_ids = [idx for idx, v in enumerate(row) if idx != sending_simplex_id and v != 0] 
-        # get connected nodes shape:[N,2]
-        connected_nodes = simplices[1][connected_simplex_ids].squeeze(1)
-        sending_simplex_nodes = simplices[1][sending_simplex_id]
-        #repeated_sending_simplex_nodes is also: [N,2]
-        repeated_sending_simplex_nodes = sending_simplex_nodes.repeat(len(connected_nodes), 1)
-        #angle calculation between edges
-        vecs = pos[simplices[1][:, 0]] - pos[simplices[1][:, 1]]
-        send_vec, rec_vec = vecs[repeated_sending_simplex_nodes], vecs[connected_nodes]
-        send_norm, rec_norm = torch.linalg.norm(send_vec), torch.linalg.norm(rec_vec)
-        dot = torch.sum(send_vec * rec_vec)
-        cos_angle = dot / (send_norm * rec_norm)
-        eps = 1e-6
-        #numerical stability
-        angle = torch.arccos(cos_angle.clamp(-1 + eps, 1 - eps))
-        # distance: 1pi - a,2pi - b,3 a - b and area
-        result_row=torch.stack([
+    
+    
+    sending_simplices = adj[1][0]
+    receiving_simplices = adj[1][1]
+    
+    connected_nodes = simplices[1][receiving_simplices].squeeze(1)
+    sending_simplex_nodes = simplices[1][sending_simplices]
+    
+    vecs = pos[simplices[1][:, 0]] - pos[simplices[1][:, 1]]
+    send_vec, rec_vec = vecs[sending_simplices], vecs[receiving_simplices]
+    send_norm, rec_norm = torch.linalg.norm(send_vec, ord=2, dim=1), torch.linalg.norm(rec_vec, ord=2, dim=1)
+    dot = torch.sum(send_vec * rec_vec)
+    cos_angle = dot / (send_norm * rec_norm)
+    eps = 1e-6
+    #numerical stability
+    angle = torch.arccos(cos_angle.clamp(-1 + eps, 1 - eps))
+    adj_dict[1] = torch.stack([
             #pi - a
-            torch.linalg.norm(pos[repeated_sending_simplex_nodes[:,0]] - pos[repeated_sending_simplex_nodes[:,1]], dim=1),
+            torch.linalg.norm(pos[sending_simplex_nodes[:,0]] - pos[sending_simplex_nodes[:,1]], dim=1),
             #pi - b
-            torch.linalg.norm(pos[repeated_sending_simplex_nodes[:,0]] - pos[connected_nodes[:,1]], dim=1),
+            torch.linalg.norm(pos[sending_simplex_nodes[:,0]] - pos[connected_nodes[:,1]], dim=1),
             #a - b
-            torch.linalg.norm(pos[repeated_sending_simplex_nodes[:,1]] - pos[connected_nodes[:,1]], dim=1),
+            torch.linalg.norm(pos[sending_simplex_nodes[:,1]] - pos[connected_nodes[:,1]], dim=1),
             #area sending (same as distance for edges)
-            torch.linalg.norm(pos[repeated_sending_simplex_nodes[:,0]] - pos[repeated_sending_simplex_nodes[:,1]], dim=1),
+            torch.linalg.norm(pos[sending_simplex_nodes[:,0]] - pos[sending_simplex_nodes[:,1]], dim=1),
             #are receiving (same as distance for edges)
-            torch.linalg.norm(pos[repeated_sending_simplex_nodes[:, 0]] - pos[connected_nodes[:, 1]], dim=1),
-            torch.full((len(connected_nodes),),angle)
-        ], dim=1)
-        result_matrix_1.append(result_row)
-    adj_dict[1] = torch.cat(result_matrix_1, dim=0)
-
+            torch.linalg.norm(pos[sending_simplex_nodes[:, 0]] - pos[connected_nodes[:, 1]], dim=1),
+            angle
+        ], dim=1)    
+    
     return adj_dict
-
 def compute_invariance_r_minus_1_to_r(simplices: Dict[int, torch.Tensor], pos: torch.Tensor, inc: Dict[int, torch.Tensor]):
     """ 
         Computes the invariances from r-cells to r-cells geometrical properties
@@ -427,68 +407,44 @@ def compute_invariance_r_minus_1_to_r(simplices: Dict[int, torch.Tensor], pos: t
     inc_dict={}
     # for 0_1 dimensional simplices:
     result_matrix_0_1 = []
-    #loop over num of 0 simplices
-    for sending_simplex_id in range(inc[0].shape[0]):
-        if sum(inc[0][sending_simplex_id]) == 0:
-            continue
-        row = inc[0][sending_simplex_id]
-        #get the indexes all connected simplices in the adjecency matrix.
-        connected_simplex_ids = [simplex_id for simplex_id, v in enumerate(row) if v != 0]
-        #assumption: second node is always higher index, to calculate distance from node to simplex is just difference between the two points
-        connected_nodes = simplices[1][connected_simplex_ids]
-        # mask = ~(connected_nodes == sending_simplex_id).any(dim=1)
-        mask = connected_nodes != sending_simplex_id
-        connected_nodes = connected_nodes[mask]
-        # create tensor of sending node for efficient feature calculation
-        zero_simplex_repeated = torch.full((len(connected_nodes),), sending_simplex_id)
-        distance = torch.linalg.norm(pos[zero_simplex_repeated] - pos[connected_nodes], dim=1)
-        features = distance.view(-1, 1).repeat(1, 3)
-        #area of the sending node is 0.
-        features[:,1] = 0
-        result_matrix_0_1.append(features)
-        
-    inc_dict[0] = torch.cat(result_matrix_0_1, dim=0)
-    result_matrix_1_2 = []
+    sending_nodes = inc[0][0]
+    receiving_simplices = inc[0][1]
+    sending_edges = inc[1][0]
+    nodes_of_receiving_simplices = simplices[1][receiving_simplices]
+    mask = nodes_of_receiving_simplices != sending_nodes.unsqueeze(1)
+    receiving_nodes = nodes_of_receiving_simplices[mask]
     
-    for sending_simplex_id in range(inc[1].shape[0]):
-        #if no connections, skip
-        if sum(inc[1][sending_simplex_id]) == 0:
-            continue
-        row = inc[1][sending_simplex_id]
-        connected_simplex_ids = [idx for idx, v in enumerate(row) if v != 0]
-        # get connected nodes shape:[N,2]
-        connected_nodes = simplices[2][connected_simplex_ids]
-        sending_simplex_nodes = simplices[1][sending_simplex_id]
-        repeated_sending_simplex_nodes = sending_simplex_nodes.repeat(len(connected_nodes), 1)
-        #we don't know in where in the triangle the node is that does not belong to the sending 1d simplex, I made a func to look it up
-        a_index = find_non_overlap(repeated_sending_simplex_nodes,connected_nodes)
-        p1, p2, a= pos[repeated_sending_simplex_nodes[:,0]], pos[repeated_sending_simplex_nodes[:,1]], pos[connected_nodes[a_index]]
-        v1, v2, b = p1 - a, p2 - a, p1 - p2
-        eps = 1e-6
-        v1_n, v2_n, b_n = torch.linalg.norm(v1, dim=1), torch.linalg.norm(v2, dim=1), torch.linalg.norm(b, dim=1)
-        v1_a = torch.arccos((torch.sum(v1 * b, dim=1) / (v1_n * b_n)).clamp(-1 + eps, 1 - eps))
-        v2_a = torch.arccos((torch.sum(v2 * b, dim=1) / (v2_n * b_n)).clamp(-1 + eps, 1 - eps))
-        b_a = torch.arccos((torch.sum(v1 * v2, dim=1) / (v1_n * v2_n)).clamp(-1 + eps, 1 - eps))
-        #check if dimensions of angle are appropriate [n,2]
-        angle = torch.moveaxis(torch.vstack((v1_a + v2_a, b_a)), 0, 1)
-        #area
-        area_1 = torch.linalg.norm(b, dim=1).unsqueeze(1)
-        area_2 = (
-            torch.norm(
-                torch.cross(
-                    pos[simplices[2][connected_simplex_ids, 0]] - pos[simplices[2][connected_simplex_ids, 1]],
-                    pos[simplices[2][connected_simplex_ids, 0]] - pos[simplices[2][connected_simplex_ids, 2]], 
-                    dim=1
-                ),
-                dim=1
-            ) / 2
-        ).unsqueeze(1)
-
-        distances = torch.stack([
+    distance = torch.linalg.norm(pos[sending_nodes] - pos[receiving_nodes], dim=1)
+    features = distance.view(-1, 1).repeat(1, 3)
+    #area of the sending node is 0.
+    features[:,1] = 0
+    inc_dict[0] = features
+    
+    receiving_triangles = inc[1][1]
+    
+    triangle_nodes = simplices[2][receiving_triangles]
+    sending_edge_nodes = simplices[1][sending_edges]   
+    a_index = find_non_overlap(sending_edge_nodes,triangle_nodes)
+    
+    p1, p2, a= pos[sending_edge_nodes[:,0]], pos[sending_edge_nodes[:,1]], pos[triangle_nodes[a_index]]
+    v1, v2, b = p1 - a, p2 - a, p1 - p2
+    eps = 1e-6
+    v1_n, v2_n, b_n = torch.linalg.norm(v1, dim=1), torch.linalg.norm(v2, dim=1), torch.linalg.norm(b, dim=1)
+    v1_a = torch.arccos((torch.sum(v1 * b, dim=1) / (v1_n * b_n)).clamp(-1 + eps, 1 - eps))
+    v2_a = torch.arccos((torch.sum(v2 * b, dim=1) / (v2_n * b_n)).clamp(-1 + eps, 1 - eps))
+    b_a = torch.arccos((torch.sum(v1 * v2, dim=1) / (v1_n * v2_n)).clamp(-1 + eps, 1 - eps))
+    #check if dimensions of angle are appropriate [n,2]
+    angle = torch.moveaxis(torch.vstack((v1_a + v2_a, b_a)), 0, 1)
+    
+    area_1 = b_n.unsqueeze(1)
+    
+    area_2 = (torch.norm(torch.cross(pos[simplices[2][receiving_triangles,0]] - pos[simplices[2][receiving_triangles, 1]],
+                                        pos[simplices[2][receiving_triangles, 0]] - pos[simplices[2][receiving_triangles, 2]], dim=1),
+                            dim=1) / 2).unsqueeze(1)
+    distances = torch.stack([
             torch.linalg.norm(p1 - a, dim=1)
             + torch.linalg.norm(p1 - a, dim=1),
             torch.linalg.norm(p2 - a, dim=1)
         ], dim=1)
-        result_matrix_1_2.append(torch.cat((distances,area_1,area_2,angle),dim=1))
-    inc_dict[1] = torch.cat(result_matrix_1_2, dim=0)
+    inc_dict[1] = torch.cat((distances,area_1,area_2,angle),dim=1)
     return inc_dict 