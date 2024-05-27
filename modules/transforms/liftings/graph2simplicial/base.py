67013900963a45df9084cc42a50151baccc1a834
Martin Carrasco <martin.carrasco@utec.edu.pe>
2024-05-27 20:35:08 +0200

    FIX: Rework to make minibatching work

--- modules/transforms/liftings/graph2simplicial/base.py
+++ modules/transforms/liftings/graph2simplicial/base.py
@@ -4,104 +4,7 @@ from toponetx.classes import SimplicialComplex
 
 from modules.data.utils.utils import get_complex_connectivity
 from modules.transforms.liftings.lifting import GraphLifting
-from modules.transforms.data_manipulations.manipulations import compute_invariance_r_minus_1_to_r, compute_invariance_r_to_r
 
-
-class Graph2InvariantSimplicialLifting(GraphLifting):
-    r"""Abstract class for lifting graphs to simplicial complexes and including 
-    invariances
-
-    Parameters
-    ----------
-    complex_dim : int, optional
-        The dimension of the simplicial complex to be generated. Default is 2.
-    **kwargs : optional
-        Additional arguments for the class.
-    """
-
-    def __init__(self, complex_dim=2, **kwargs):
-        super().__init__(**kwargs)
-        self.complex_dim = complex_dim
-        self.type = "graph2simplicial"
-        self.signed = kwargs.get("signed", False)
-
-    def _get_lifted_topology(
-        self, simplicial_complex: SimplicialComplex, graph: nx.Graph
-    ) -> dict:
-        r"""Returns the lifted topology.
-
-        Parameters
-        ----------
-        simplicial_complex : SimplicialComplex
-            The simplicial complex.
-        graph : nx.Graph
-            The input graph.
-
-        Returns
-        -------
-        dict
-            The lifted topology.
-        """
-        lifted_topology = get_complex_connectivity(
-            simplicial_complex, self.complex_dim, signed=self.signed
-        )
-
-        feat_ind = {}
-        adj_dict = {}
-        inc_dict = {}
-
-        # Create feature dictionary
-        for r in range(0, simplicial_complex.dim+1):
-            feat_ind[r] = torch.tensor(simplicial_complex.skeleton(r), dtype=torch.int)
-            lifted_topology[f'x_idx_{r}'] =feat_ind[r]
-            adj_dict[r] = lifted_topology[f'adjacency_{r}']
-
-        '''
-        # For some reason this incidence marix never has problems ?
-        # Remove incidence relations where the symmetric difference between (r-1)-cell and r-cell > 1
-        for r in range(1, simplicial_complex.dim+1):
-            r_inc = lifted_topology[f'incidence_{r}'].to_dense()
-            for i in range(0, feat_ind[r-1].size(0)):
-                for j in range(0, feat_ind[r].size(0)):
-                    if r_inc[i][j] == 0:
-                        continue
-
-                    set_1 = set(feat_ind[r-1][i].tolist())
-                    set_2 = set(feat_ind[r][j].tolist())
-
-                    sym_dif = set(set_1).symmetric_difference(set(set_2))
-                    if len(sym_dif) > 1:
-                        r_inc[i][j] = r_inc[i][j] = 0
-            lifted_topology[f'incidence_{r}'] = r_inc.to_sparse()
-        '''
-
-        # Assign r-cell the incidences of (r+1)-cell so that we can compute invariances
-        for r in range(0, simplicial_complex.dim):
-            inc_dict[r] = lifted_topology[f'incidence_{r+1}']
-
-        inv_same_dict = compute_invariance_r_to_r(feat_ind, graph.pos, adj_dict)
-        inv_low_high_dict = compute_invariance_r_minus_1_to_r(feat_ind, graph.pos, inc_dict)
-
-        # Set invariances in data
-        # Fix for the mismatch in computing invariances above
-        for r in range(0, simplicial_complex.dim+1):
-            if r != simplicial_complex.dim:
-                lifted_topology[f'inv_same_{r}'] = inv_same_dict[r]
-            if r > 0:
-                lifted_topology[f'inv_low_high_{r}'] = inv_low_high_dict[r-1]
-
-        # Set features of the 0-cell
-        lifted_topology["x_0"] = torch.stack(
-            list(simplicial_complex.get_simplex_attributes("features", 0).values())
-        )
-        # If new edges have been added during the lifting process, we discard the edge attributes
-        if self.contains_edge_attr and simplicial_complex.shape[1] == (
-            graph.number_of_edges()
-        ):
-            lifted_topology["x_1"] = torch.stack(
-                list(simplicial_complex.get_simplex_attributes("features", 1).values())
-            )
-        return lifted_topology
 
 
 
@@ -142,13 +45,6 @@ class Graph2SimplicialLifting(GraphLifting):
         lifted_topology = get_complex_connectivity(
             simplicial_complex, self.complex_dim, signed=self.signed
         )
-
-        feat_ind = {}
-
-        # Create feature dictionary
-        for r in range(0, simplicial_complex.dim+1):
-            feat_ind[r] = torch.tensor(simplicial_complex.skeleton(r), dtype=torch.int)
-            lifted_topology[f'x_idx_{r}'] = feat_ind[r]
 
         # Set features of the 0-cell
         lifted_topology["x_0"] = torch.stack(
--- modules/transforms/liftings/graph2simplicial/vietoris_rips_lift.py
+++ modules/transforms/liftings/graph2simplicial/vietoris_rips_lift.py
@@ -7,7 +7,10 @@ from gudhi import SimplexTree
 from toponetx.classes import SimplicialComplex
 import torch
 
-from modules.transforms.liftings.graph2simplicial.base import Graph2SimplicialLifting, Graph2InvariantSimplicialLifting
+
+from modules.data.utils.utils import get_complex_connectivity
+from modules.transforms.liftings.graph2simplicial.base import Graph2SimplicialLifting
+from modules.transforms.data_manipulations.manipulations import compute_invariance_r_minus_1_to_r, compute_invariance_r_to_r
 
 
 def rips_lift(graph: torch_geometric.data.Data, dim: int, dis: float, fc_nodes: bool = True) -> SimplicialComplex:
@@ -32,24 +35,46 @@ def rips_lift(graph: torch_geometric.data.Data, dim: int, dis: float, fc_nodes: 
 
     return SimplicialComplex.from_gudhi(simplex_tree)
 
-class InvariantSimplicialVietorisRipsLifting(Graph2InvariantSimplicialLifting):
-    r"""Lifts graphs to simplicial complex domain by identifying the cliques as k-simplices.
+class SimplexData(torch_geometric.data.Data):
+    def __inc__(self, key: str, value, *args, **kwargs):
+        if 'adjacency' in key:
+            rank = int(key.split('_')[-1])
+            return torch.tensor([getattr(self, f'x_{rank}').size(0)])
+            #return torch.tensor([[getattr(self, f'x_{rank}').size(0)], [getattr(self, f'x_{rank}').size(0)]])
+        elif 'incidence' in key:
+            rank = int(key.split('_')[-1])
+            if rank == 0:
+                return torch.tensor([getattr(self, f'x_{rank}').size(0)])
+            return torch.tensor([[getattr(self, f'x_{rank-1}').size(0)], [getattr(self, f'x_{rank}').size(0)]])
+        elif key == 'x_0' or key == 'x_idx_0':
+            return torch.tensor([getattr(self, f'x_0').size(0)])
+        elif key == 'x_1' or key == 'x_idx_1':
+            return torch.tensor([getattr(self, f'x_0').size(0)])
+        elif key == 'x_2' or key == 'x_idx_2':
+            return torch.tensor([getattr(self, f'x_0').size(0)])
+        elif 'index' in key:
+            return self.num_nodes
+        else:
+            return super().__inc__(key, value, *args, **kwargs)
 
-    Parameters
-    ----------
-    distance : int, optional
-        The distance for the Vietoris-Rips complex. Default is 0.
-    **kwargs : optional
-        Additional arguments for the class.
+    def __cat_dim__(self, key: str, value, *args, **kwargs):
+        if 'adjacency' in key or 'incidence' in key or 'index' in key:
+            return 1
+        else:
+            return 0
+
+
+
+class BaseSimplicialVRLifting(Graph2SimplicialLifting):
+    r""" Base class for lifting graphs to simplicial complexes using Vietoris-Rips complex.
     """
-
     def __init__(self, dis: float = 0, **kwargs):
         super().__init__(**kwargs)
         self.dis = dis
-        self.contains_edge_attr = None
 
     def lift_topology(self, data: torch_geometric.data.Data) -> dict:
-        r"""Lifts the topology of a graph to a simplicial complex by identifying the cliques as k-simplices.
+        r"""Lifts the topology of a graph to a simplicial complex by using the Vietoris-Rips lift
+        and assigns the features of the 0-rank simplices.
 
         Parameters
         ----------
@@ -75,7 +100,26 @@ class InvariantSimplicialVietorisRipsLifting(Graph2InvariantSimplicialLifting):
         simplicial_complex.set_simplex_attributes(feature_dict, name='features')
 
         return self._get_lifted_topology(simplicial_complex, data)
-class SimplicialVietorisRipsLifting(Graph2SimplicialLifting):
+
+    def forward(self, data: torch_geometric.data.Data) -> torch_geometric.data.Data:
+        r"""Applies the full lifting (topology + features) to the input data.
+        and incorporates the SimplexData structure for mini-batching.
+
+        Parameters
+        ----------
+        data : torch_geometric.data.Data
+            The input data to be lifted.
+
+        Returns
+        -------
+        torch_geometric.data.Data
+            The lifted data.
+        """
+        initial_data = data.to_dict()
+        lifted_topology = self.lift_topology(data)
+        lifted_topology = self.feature_lifting(lifted_topology)
+        return SimplexData(**initial_data, **lifted_topology)
+class InvariantSimplicialVietorisRipsLifting(BaseSimplicialVRLifting):
     r"""Lifts graphs to simplicial complex domain by identifying the cliques as k-simplices.
 
     Parameters
@@ -86,35 +130,109 @@ class SimplicialVietorisRipsLifting(Graph2SimplicialLifting):
         Additional arguments for the class.
     """
 
-    def __init__(self, dis: float = 0, **kwargs):
-        super().__init__(**kwargs)
-        self.dis = dis
-        self.contains_edge_attr = None
-
-    def lift_topology(self, data: torch_geometric.data.Data) -> dict:
-        r"""Lifts the topology of a graph to a simplicial complex by identifying the cliques as k-simplices.
+    def _get_lifted_topology(
+        self, simplicial_complex: SimplicialComplex, graph: nx.Graph
+    ) -> dict:
+        r"""Returns the lifted topology.
 
         Parameters
         ----------
-        data : torch_geometric.data.Data
-            The input data to be lifted.
+        simplicial_complex : SimplicialComplex
+            The simplicial complex.
+        graph : nx.Graph
+            The input graph.
 
         Returns
         -------
         dict
             The lifted topology.
         """
+        lifted_topology = get_complex_connectivity(
+            simplicial_complex, self.complex_dim, signed=self.signed
+        )
+
+        feat_ind = {}
+        adj_dict = {}
+        inc_dict = {}
+
+        # Create feature dictionary
+        for r in range(0, simplicial_complex.dim+1):
+            lifted_topology[f'x_idx_{r}'] = feat_ind[r] = torch.tensor(simplicial_complex.skeleton(r), dtype=torch.int)
+            lifted_topology[f'adjacency_{r}'] = adj_dict[r] = lifted_topology[f'adjacency_{r}'].to_dense().nonzero().t().contiguous()
+            lifted_topology[f'incidence_{r}'] = lifted_topology[f'incidence_{r}'].to_dense().nonzero().t().contiguous()
+
+        # Assign r-cell the incidences of (r+1)-cell so that we can compute invariances
+        for r in range(0, simplicial_complex.dim):
+            inc_dict[r] = lifted_topology[f'incidence_{r+1}']
+
+        inv_same_dict = compute_invariance_r_to_r(feat_ind, graph.pos, adj_dict)
+        inv_low_high_dict = compute_invariance_r_minus_1_to_r(feat_ind, graph.pos, inc_dict)
+
+        # Set invariances in data
+        # Fix for the mismatch in computing invariances above
+        for r in range(0, simplicial_complex.dim+1):
+            if r != simplicial_complex.dim:
+                lifted_topology[f'inv_same_{r}'] = inv_same_dict[r]
+            if r > 0:
+                lifted_topology[f'inv_low_high_{r}'] = inv_low_high_dict[r-1]
+
+        # Set features of the 0-cell
+        lifted_topology["x_0"] = torch.stack(
+            list(simplicial_complex.get_simplex_attributes("features", 0).values())
+        )
+        return lifted_topology
 
 
-        # Lift graph to Simplicial Complex
-        simplicial_complex = rips_lift(data, self.complex_dim, self.dis)
 
-        # Retrieve features as a directory
-        feature_dict = {}
-        for i, node in enumerate(data.x):
-            feature_dict[i] = node
 
-        # Encode features in the simplex
-        simplicial_complex.set_simplex_attributes(feature_dict, name='features')
 
-        return self._get_lifted_topology(simplicial_complex, data)
+
+
+class SimplicialVietorisRipsLifting(BaseSimplicialVRLifting):
+    r"""Lifts graphs to simplicial complex domain by identifying the cliques as k-simplices.
+
+    Parameters
+    ----------
+    distance : int, optional
+        The distance for the Vietoris-Rips complex. Default is 0.
+    **kwargs : optional
+        Additional arguments for the class.
+    """
+
+    def _get_lifted_topology(
+        self, simplicial_complex: SimplicialComplex, graph: nx.Graph
+    ) -> dict:
+        r"""Returns the lifted topology.
+
+        Parameters
+        ----------
+        simplicial_complex : SimplicialComplex
+            The simplicial complex.
+        graph : nx.Graph
+            The input graph.
+
+        Returns
+        -------
+        dict
+            The lifted topology.
+        """
+        lifted_topology = get_complex_connectivity(
+            simplicial_complex, self.complex_dim, signed=self.signed
+        )
+
+        # Transform matrices to edge_index format
+        for r in range(0, simplicial_complex.dim+1):
+            lifted_topology[f'adjacency_{r}'] = lifted_topology[f'adjacency_{r}'].to_dense().nonzero().t().contiguous()
+            lifted_topology[f'incidence_{r}'] = lifted_topology[f'incidence_{r}'].to_dense().nonzero().t().contiguous()
+
+
+        # Create feature dictionary
+        for r in range(0, simplicial_complex.dim+1):
+            lifted_topology[f'x_idx_{r}'] = torch.tensor(simplicial_complex.skeleton(r), dtype=torch.int)
+
+        # Set features of the 0-cell
+        lifted_topology["x_0"] = torch.stack(
+            list(simplicial_complex.get_simplex_attributes("features", 0).values())
+        )
+
+        return lifted_topology
