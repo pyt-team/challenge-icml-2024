Fixed numerical issues in tests.

test/transforms/liftings/graph2cell/test_neighborhood.py
@@ -26,16 +26,15 @@ class TestCellCyclesLifting:
         expected_incidence_1_singular_values = torch.tensor(
             [3.4431, 2.4495, 2.4495, 2.3984, 2.2361, 2.2050, 1.9275, 1.6779]
         )
-
-        assert (
-            expected_incidence_1_singular_values == S
-        ).all(), "Something is wrong with incidence_1."
+        assert torch.allclose(
+            expected_incidence_1_singular_values, S, atol=1e-4
+        ), "Something is wrong with incidence_1."
 
         U, S, V = torch.svd(lifted_data.incidence_2.to_dense())
         expected_incidence_2_singular_values = torch.tensor(
             [3.8155, 3.0758, 2.5256, 2.3475, 1.8136, 1.5562, 1.3854, 1.2090]
         )
 
-        assert (
-            expected_incidence_2_singular_values == S
-        ).all(), "Something is wrong with incidence_2."
+        assert torch.allclose(
+            expected_incidence_2_singular_values, S, atol=1e-4
+        ), "Something is wrong with incidence_2."