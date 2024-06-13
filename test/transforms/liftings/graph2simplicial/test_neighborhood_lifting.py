169a2bc7dbf257635e4a4129844fd35ed289a64d
Snopoff <snopoffp@gmail.com>
2024-06-13 12:27:34 +0300

    Added tests

--- test/transforms/liftings/graph2simplicial/test_neighborhood_lifting.py
+++ test/transforms/liftings/graph2simplicial/test_neighborhood_lifting.py
@@ -1,6 +1,7 @@
 """Test the message passing module."""
 
 import torch
+import torch_geometric
 
 from modules.data.utils.utils import load_manual_graph
 from modules.transforms.liftings.graph2simplicial.neighborhood_lifting import (
@@ -8,13 +9,26 @@ from modules.transforms.liftings.graph2simplicial.neighborhood_lifting import (
 )
 
 
+def create_test_graph():
+    num_nodes = 5
+    x = [1] * num_nodes
+    edge_index = [[0, 0, 1, 1, 2, 2, 3], [1, 4, 2, 3, 3, 4, 4]]
+    y = [0, 0, 1, 1, 0]
+
+    return torch_geometric.data.Data(
+        x=torch.tensor(x).float().reshape(-1, 1),
+        edge_index=torch.Tensor(edge_index),
+        num_nodes=num_nodes,
+        y=torch.tensor(y),
+    )
+
+
 class TestSimplicialCliqueLifting:
     """Test the SimplicialCliqueLifting class."""
 
     def setup_method(self):
         # Load the graph
-        self.data = load_manual_graph()
-        print(self.data)
+        self.data = create_test_graph()  # load_manual_graph()
 
         # Initialise the SimplicialCliqueLifting class
         self.lifting_signed = NeighborhoodComplexLifting(signed=True)
@@ -26,21 +40,17 @@ class TestSimplicialCliqueLifting:
         # Test the lift_topology method
         lifted_data_signed = self.lifting_signed.forward(self.data.clone())
         lifted_data_unsigned = self.lifting_unsigned.forward(self.data.clone())
+        print(lifted_data_signed)
 
         expected_incidence_1 = torch.tensor(
             [
-                [-1.0, -1.0, -1.0, -1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-                [1.0, 0.0, 0.0, 0.0, -1.0, -1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-                [0.0, 1.0, 0.0, 0.0, 1.0, 0.0, -1.0, -1.0, -1.0, -1.0, 0.0, 0.0, 0.0],
-                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, -1.0, 0.0, 0.0],
-                [0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, -1.0, -1.0],
-                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0],
-                [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0],
+                [-1.0, -1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                [0.0, 0.0, -1.0, -1.0, -1.0, 0.0, 0.0, 0.0],
+                [1.0, 0.0, 1.0, 0.0, 0.0, -1.0, -1.0, 0.0],
+                [0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, -1.0],
+                [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0],
             ]
         )
-
-        print(lifted_data_unsigned.incidence_1.to_dense())
 
         assert (
             abs(expected_incidence_1) == lifted_data_unsigned.incidence_1.to_dense()
@@ -51,40 +61,20 @@ class TestSimplicialCliqueLifting:
 
         expected_incidence_2 = torch.tensor(
             [
-                [1.0, 1.0, 0.0, 0.0, 0.0, 0.0],
-                [-1.0, 0.0, 1.0, 1.0, 0.0, 0.0],
-                [0.0, -1.0, -1.0, 0.0, 0.0, 0.0],
-                [0.0, 0.0, 0.0, -1.0, 0.0, 0.0],
-                [1.0, 0.0, 0.0, 0.0, 1.0, 0.0],
-                [0.0, 1.0, 0.0, 0.0, -1.0, 0.0],
-                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-                [0.0, 0.0, 1.0, 0.0, 1.0, 0.0],
-                [0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
-                [0.0, 0.0, 0.0, 1.0, 0.0, -1.0],
-                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-                [0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+                [1.0, 0.0, 0.0],
+                [-1.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0],
+                [0.0, 0.0, 1.0],
+                [0.0, -1.0, -1.0],
+                [1.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0],
+                [0.0, 0.0, 1.0],
             ]
         )
 
         assert (
             abs(expected_incidence_2) == lifted_data_unsigned.incidence_2.to_dense()
-        ).all(), "Something is wrong with unsigned incidence_2 (edges to triangles)."
+        ).all(), f"Something is wrong with unsigned incidence_2 (edges to triangles).\n{abs(expected_incidence_2) - lifted_data_unsigned.incidence_2.to_dense()}"
         assert (
             expected_incidence_2 == lifted_data_signed.incidence_2.to_dense()
-        ).all(), "Something is wrong with signed incidence_2 (edges to triangles)."
-
-        expected_incidence_3 = torch.tensor(
-            [[-1.0], [1.0], [-1.0], [0.0], [1.0], [0.0]]
-        )
-
-        assert (
-            abs(expected_incidence_3) == lifted_data_unsigned.incidence_3.to_dense()
-        ).all(), (
-            "Something is wrong with unsigned incidence_3 (triangles to tetrahedrons)."
-        )
-        assert (
-            expected_incidence_3 == lifted_data_signed.incidence_3.to_dense()
-        ).all(), (
-            "Something is wrong with signed incidence_3 (triangles to tetrahedrons)."
-        )
+        ).all(), f"Something is wrong with signed incidence_2 (edges to triangles).\n{lifted_data_signed.incidence_2.to_dense()}"
