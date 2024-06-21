e1a556b2aabece8e6975fb5ded188144f32bacde
Bertran Miquel Oliver <bertranmiqueloliver@gmail.com>
2024-06-21 14:33:02 +0200

    Update incidence hypergraph matrix

---
+++ modules/transforms/liftings/graph2combinatorial/functional_lifting.py
@@ -0,0 +1,161 @@
+import torch
+import torch_geometric
+from rdkit import Chem
+from toponetx.classes import CellComplex
+
+from modules.transforms.liftings.graph2cell.base import Graph2CellLifting
+from modules.transforms.liftings.graph2combinatorial.base import (
+    Graph2CombinatorialLifting,
+)
+
+
+class CombinatorialFunctionalLifting(Graph2CombinatorialLifting):
+    r"""Lifts r-cell features to r+1-cells by molecular rings.
+    This rings are obtained by chemical knowledge from rdkit.
+    Moreover, the functional groups are added as hyperedges.
+    Functional groups are defined by SMARTS patterns in the
+    corresponding lifting config file.
+
+    Parameters
+    ----------
+    **kwargs : optional
+        Additional arguments for the class.
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.complex_dim = 2
+
+        self.functional_groups = kwargs.get("functional_groups", [])
+
+    def _generate_mol_from_data(
+            self, data: torch_geometric.data.Data | dict
+        ) -> Chem.Mol:
+        r"""Converts the data to a molecule through the SMILES
+            and removes the hydrogens.
+
+        Parameters
+        ----------
+        data : torch_geometric.data.Data
+            The input data.
+
+        Returns
+        -------
+        Chem.Mol
+            The molecule.
+        """
+        mol = Chem.MolFromSmiles(data.smiles)
+        return Chem.rdmolops.RemoveHs(mol)
+
+    #######################################################
+    ################### RINGS #############################
+    #######################################################
+    def get_rings(
+            self, mol: Chem.Mol
+        ) -> torch.Tensor:
+        r"""Returns the ring information for each molecule.
+
+        Parameters
+        ----------
+        mol : Chem.Mol
+            The molecule.
+
+        Returns
+        -------
+        torch.Tensor
+            The ring information.
+        """
+
+        return [list(ring) for ring in Chem.GetSymmSSSR(mol)]
+
+    #######################################################
+    ############## FUNCTIONAL GROUPS ######################
+    #######################################################
+
+    def get_functional_groups(
+        self, mol: Chem.Mol
+    ) -> dict:
+        r"""Returns the functional groups for each molecule.
+
+        Parameters
+        ----------
+        mol : Chem.Mol
+            The molecule.
+
+        Returns
+        -------
+        dict
+
+        """
+
+        # return {fg: [list(match) for match in mol.GetSubstructMatches(Chem.MolFromSmarts(fg))]
+        #             for fg in self.functional_groups if Chem.MolFromSmarts(fg)}
+
+        cliques = {}
+        for fg in self.functional_groups:
+            fg_mol = Chem.MolFromSmarts(fg)
+            if fg_mol:
+                matches = mol.GetSubstructMatches(fg_mol)
+                if matches:
+                    cliques[fg] = [list(match) for match in matches]
+
+        return cliques
+
+    #######################################################
+    ################### LIFT ##############################
+    #######################################################
+    def lift_topology(
+        self, data: torch_geometric.data.Data | dict
+    ) -> torch_geometric.data.Data | dict:
+        r"""Lifts the topology of a graph to a combinatorial complex.
+        Following the steps:
+        1. Convert the data to a molecule and remove the hydrogens.
+        2. Generate the graph from the molecule. Take into account that this graph
+            will not be the same as the starting one, since the hydrogens are removed.
+        3. Generate the cell complex from the graph adding the rings as 2-cells.
+        4. Add functional groups as hyperedges.
+
+        Parameters
+        ----------
+        data : torch_geometric.data.Data | dict
+            The input data to be lifted.
+
+        Returns
+        -------
+        torch_geometric.data.Data | dict
+            The lifted data."""
+
+        # Combinaotrial complex is a combination of a cell complex and a hypergraph
+        # First create a cell complex and then add hyperedges
+
+        G = self._generate_graph_from_data(data)
+        ccc = CellComplex(G)
+        mol = self._generate_mol_from_data(data)
+
+        # add rings as 2-cells
+        rings = self.get_rings(mol)
+        for ring in rings:
+            ccc.add_cell(ring, rank=self.complex_dim)
+
+        # Hypergraph stuff
+        # add functional groups as hyperedges (rank = 1)
+        cliques = self.get_functional_groups(mol)
+        num_hyperedges = len(cliques)
+        # create incidence matrix for hyperedges
+        incidence_1 = torch.zeros(data.num_nodes, num_hyperedges)
+        for i, group in enumerate(cliques.values()):
+            for hyperedge in group:
+                for atom in hyperedge:
+                    incidence_1[atom, i] = 1
+
+        # Create the lifted topology dict for the cell complex
+        ccc_lifted_topology = Graph2CellLifting._get_lifted_topology(self, ccc, G)
+
+        # add hyperedges to the lifted topology
+        ccc_lifted_topology["num_hyperedges"] = num_hyperedges
+        ccc_lifted_topology["x_0"] = data.x
+        ccc_lifted_topology["incidence_hyperedges"] = torch.Tensor(
+            incidence_1
+        ).to_sparse_coo()
+
+        return ccc_lifted_topology
