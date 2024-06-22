71bbbc1fce310165274257d1f258b697ca13961b
MatousE <matous.elphick@gmail.com>
2024-06-22 11:26:13 +0100

    Feat - adding _get_lifted_topology function to return topology of the splicial complex

--- modules/transforms/liftings/pointcloud2simplicial/base.py
+++ modules/transforms/liftings/pointcloud2simplicial/base.py
@@ -1,3 +1,7 @@
+import torch
+from toponetx.classes import SimplicialComplex
+
+from modules.data.utils.utils import get_complex_connectivity
 from modules.transforms.liftings.lifting import PointCloudLifting
 
 
@@ -16,3 +20,28 @@ class PointCloud2SimplicialLifting(PointCloudLifting):
         super().__init__(**kwargs)
         self.complex_dim = complex_dim
         self.type = "pointcloud2simplicial"
+
+    def _get_lifted_topology(self, simplicial_complex: SimplicialComplex) -> dict:
+        r"""Returns the lifted topology.
+
+        Parameters
+        ----------
+        simplicial_complex : SimplicialComplex
+            The simplicial complex.
+        graph : nx.Graph
+            The input graph.
+
+        Returns
+        -------
+        dict
+            The lifted topology.
+        """
+        lifted_topology = get_complex_connectivity(
+            simplicial_complex, simplicial_complex.maxdim
+        )
+
+        lifted_topology["x_0"] = torch.stack(
+            list(simplicial_complex.get_simplex_attributes("features", 0).values())
+        )
+
+        return lifted_topology
