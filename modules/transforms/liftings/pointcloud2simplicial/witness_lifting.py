f435e4e5a5d13854d764cc40ea020c6609f5e5d4
Germandev55 <gera303813_96@mail.ru>
2024-06-27 04:03:53 +0900

    Update witness_lifting.py

--- modules/transforms/liftings/pointcloud2simplicial/witness_lifting.py
+++ modules/transforms/liftings/pointcloud2simplicial/witness_lifting.py
@@ -8,9 +8,9 @@ from modules.transforms.liftings.pointcloud2simplicial.base import (
 
 
 class WitnessLifting(PointCloud2SimplicialLifting):
-    def __init__(self, is_weak=True, is_euclidian=True, **kwargs):
+    def __init__(self, is_weat=True, is_euclidian=True, **kwargs):
         super().__init__(**kwargs)
-        self.is_weak = is_weak
+        self.is_weat = is_weat
         self.is_euclidian = is_euclidian
 
     def lift_topology(self, data: torch_geometric.data.Data) -> dict:
