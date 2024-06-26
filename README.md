3102683664b6d5b67056f62c7f2dcdc195379265
Bertran Miquel Oliver <bertranmiqueloliver@gmail.com>
2024-06-26 18:09:06 +0200

    Implementing attributes...

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# Molecule Ring & Close Atoms Lifting (Graph to Combinatorial)
+# Molecule Ring & Functional Lifting (Graph to Combinatorial)
 This notebook imports QM9 dataset and applies a lifting from a graph molecular representation to a combinatorial complex. Then, a neural network is run using the loaded data.
 
 Using [QM9 dataset](https://paperswithcode.com/dataset/qm9), we implement a lifting from a molecule graph to a combinatorial complex based on two points:
 - The ring information of the molecule. Rings will be represented as 2-cells in the combinatorial complex.
 - Its functional groups will be add as hyperedges in the complex, i.e., 1-cells that sometimes connect more than two 0-cells. The functional groups are found by the SMARTS patterns, predifenied patterns that are used to identify functional groups in molecules.
 
-So far, from our knowledge it is the first representation of a cell as a combinatorial complex, combining both hypergraphs and cell complexes.
+So far, to the best of our knowledge, it is the first implementation of a molecule as a combinatorial complex, combining both hypergraphs and cell complexes.
 
 Here, the elements are the following:
 - **Nodes**: Atoms in the molecule.
--- modules/transforms/liftings/graph2combinatorial/functional_lifting.py
+++ modules/transforms/liftings/graph2combinatorial/functional_lifting.py
@@ -1,7 +1,8 @@
 import torch
 import torch_geometric
-from rdkit import Chem
 from toponetx.classes import CellComplex
+from rdkit import Chem
+from rdkit.Chem import Descriptors
 
 from modules.transforms.liftings.graph2cell.base import Graph2CellLifting
 from modules.transforms.liftings.graph2combinatorial.base import (
@@ -84,13 +85,16 @@ class CombinatorialFunctionalLifting(Graph2CombinatorialLifting):
 
         Returns
         -------
-        dict
+        dict of list
+            The functional groups with the atom ids.
+            Example:
+            {
+                "C(=O)O": [[0, 1, 2], [2, 3, 9]],
+                "C(=O)N": [[3, 4, 5], [5, 6, 7]],
+                ...
+            }
 
         """
-
-        # return {fg: [list(match) for match in mol.GetSubstructMatches(Chem.MolFromSmarts(fg))]
-        #             for fg in self.functional_groups if Chem.MolFromSmarts(fg)}
-
         cliques = {}
         for fg in self.functional_groups:
             fg_mol = Chem.MolFromSmarts(fg)
@@ -104,6 +108,7 @@ class CombinatorialFunctionalLifting(Graph2CombinatorialLifting):
     #######################################################
     ################### ATTRIBUTES ########################
     #######################################################
+
     def get_atom_attributes(
             self, mol: Chem.Mol
         ) -> dict:
@@ -117,7 +122,16 @@ class CombinatorialFunctionalLifting(Graph2CombinatorialLifting):
         Returns
         -------
         dict
-            The atom attributes.
+            The atom attributes with the atom id.
+            Example:
+            {
+                0: {"atomic_num": 6, "symbol": "C", "degree": 3, "formal_charge": 0, "hybridization": "SP
+                    "is_aromatic": False, "mass": 12.01, "chirality": "CHI_UNSPECIFIED"},
+
+                1: {"atomic_num": 6, "symbol": "C", "degree": 2, "formal_charge": 0, "hybridization": "SP
+                    "is_aromatic": False, "mass": 12.01, "chirality": "CHI_UNSPECIFIED"},
+                ...
+            }
         """
         attr = {}
         for atom in mol.GetAtoms():
@@ -146,7 +160,13 @@ class CombinatorialFunctionalLifting(Graph2CombinatorialLifting):
         Returns
         -------
         dict
-            The bond attributes.
+            The bond attributes with the bond id.
+            Example:
+            {
+                (0, 1): {"bond_type": "SINGLE", "is_conjugated": False, "is_stereo": False},
+                (1, 2): {"bond_type": "DOUBLE", "is_conjugated": False, "is_stereo": False},
+                ...
+            }
         """
         attr = {}
         for bond in mol.GetBonds():
@@ -158,26 +178,135 @@ class CombinatorialFunctionalLifting(Graph2CombinatorialLifting):
         return attr
 
     def get_ring_attributes(
-            self, mol: Chem.Mol
+            self, mol: Chem.Mol, rings: list
         ) -> dict:
         r"""Returns the ring attributes for each molecule.
+
+        • Ring Size: Number of atoms that form the ring.
+        • Aromaticity: A binary feature telling whether the ring is aromatic.
+        • Has Heteroatom: A binary feature telling whether the ring contains a heteroatom,
+          which is an atom other than carbon, such as nitrogen, oxygen, or sulfur.
+        • Saturation: A binary feature telling whether the ring is saturated, meaning all
+          the bonds between carbon atoms are single bonds.
+        • Hydrophobicity: Tendency of the functional group to repel water.
+             Compute based in logp.
+        • Electrophilicity: Tendency of the functional group to accept electrons during a chemical reaction.
+             Compute based on the number of hydrogen bonds acceptors.
+        • Nucleophilicity: Tendency of the functional group to donate electrons during a chemical reaction.
+             High nucleophilicity means the functional group readily donates electrons, affecting its reactivity
+             with electrophiles. Compute based in number of hydrogen bond donors.
+        • Polarity: Distribution of electron density within the functional group. High polarity can significantly
+             affect solubility, reactivity, and interactions with other molecules.
+             Compute based on the tpsa.
+        Parameters
+        ----------
+        mol : Chem.Mol
+            The molecule.
+
+        rings : list
+            The rings of the molecule.
+
+        Returns
+        -------
+        dict
+            The ring attributes.
+            Example:
+            {
+                (0, 1, 2): {"ring_size": 3, "aromaticity": False, "has_heteroatom": False, "saturation": True},
+                (1, 2, 3): {"ring_size": 3, "aromaticity": False, "has_heteroatom": False, "saturation": True},
+                ...
+            }
+        """
+        attr = {}
+        for ring in rings:
+            ring_size = len(ring)
+            aromaticity = all([atom.GetIsAromatic() for atom in ring])
+            has_heteroatom = any([atom.GetAtomicNum() != 6 for atom in ring])
+            saturation = all([bond.GetBondType() == Chem.rdchem.BondType.SINGLE for bond in ring])
+
+            fg_mol = Chem.MolFromSmiles("".join([mol.GetAtomWithIdx(atom).GetSymbol() for atom in ring]))
+            hydrophobicity = Descriptors.MolLogP(fg_mol)
+            electrophilicity = Descriptors.NumHAcceptors(fg_mol)
+            nucleophilicity = Descriptors.NumHDonors(fg_mol)
+            polarity = Descriptors.TPSA(fg_mol)
+
+            attr[tuple(ring)] = {
+                "ring_size": ring_size,
+                "aromaticity": aromaticity,
+                "has_heteroatom": has_heteroatom,
+                "saturation": saturation,
+                "hydrophobicity": hydrophobicity,
+                "electrophilicity": electrophilicity,
+                "nucleophilicity": nucleophilicity,
+                "polarity": polarity
+            }
+
+        return attr
+
+    def get_functional_groups_attributes(
+            self, mol: Chem.Mol, cliques: dict
+        ) -> dict:
+        r"""Returns the functional groups attributes for each molecule.
+
+        • Conjugation: A binary feature telling whether the functional group
+            is part of a conjugated system, which involves alternating double and single bonds.
+        • Hydrophobicity: Tendency of the functional group to repel water.
+             Compute based in logp.
+        • Electrophilicity: Tendency of the functional group to accept electrons during a chemical reaction.
+             Compute based on the number of hydrogen bonds acceptors.
+        • Nucleophilicity: Tendency of the functional group to donate electrons during a chemical reaction.
+             High nucleophilicity means the functional group readily donates electrons, affecting its reactivity
+             with electrophiles. Compute based in number of hydrogen bond donors.
+        • Polarity: Distribution of electron density within the functional group. High polarity can significantly
+             affect solubility, reactivity, and interactions with other molecules.
+             Compute based on the tpsa.
 
         Parameters
         ----------
         mol : Chem.Mol
             The molecule.
 
+        cliques : dict
+            The functional groups of the molecule.
+
         Returns
         -------
         dict
-            The ring attributes.
+            The functional groups attributes.
+            Example:
+            {
+                (0, 1, 2): {"functional_group": "C(=O)O", "num_atoms": 3, "conjugation": False, "acidity": "high",
+                    "hydrophobicity": "moderate", "electrophilicity": "low", "nucleophilicity": "low", "polarity": "low"},
+                (2, 3, 9): {"functional_group": "C(=O)O", "num_atoms": 3, "conjugation": False, "acidity": "neutral",
+                    "hydrophobicity": "moderate", "electrophilicity": "low", "nucleophilicity": "low", "polarity": "low"},
+                ...
+            }
         """
-        # not implemented yet
+
+        attr = {}
+        for fg, atoms in cliques.items():
+            fg_mol = Chem.MolFromSmarts(fg)
+            conjugation = any([atom.GetIsConjugated() for atom in fg_mol.GetAtoms()])
+            hydrophobicity = Descriptors.MolLogP(fg_mol)
+            electrophilicity = Descriptors.NumHAcceptors(fg_mol)
+            nucleophilicity = Descriptors.NumHDonors(fg_mol)
+            polarity = Descriptors.TPSA(fg_mol)
+
+            attr[tuple(atoms)] = {
+                "functional_group": fg,
+                "num_atoms": len(atoms),
+                "conjugation": conjugation,
+                "hydrophobicity": hydrophobicity,
+                "electrophilicity": electrophilicity,
+                "nucleophilicity": nucleophilicity,
+                "polarity": polarity
+            }
         return {}
 
     #######################################################
     ################### LIFT ##############################
     #######################################################
+
     def lift_topology(
         self, data: torch_geometric.data.Data | dict
     ) -> torch_geometric.data.Data | dict:
@@ -219,19 +348,27 @@ class CombinatorialFunctionalLifting(Graph2CombinatorialLifting):
         for ring in rings:
             ccc.add_cell(ring, rank=self.complex_dim)
 
-        ring_attr = self.get_ring_attributes(mol)
+        ring_attr = self.get_ring_attributes(mol, rings)
         ccc.set_cell_attributes(ring_attr)
 
         # Hypergraph stuff
-        # add functional groups as hyperedges (rank = 1)
+        # add functional groups and edges as hyperedges (rank = 1)
+        # edges are like this: [[0, 1], [1, 2], ...]
+        # cliques are like this: {"C(=O)O": [[0, 1, 2], [2, 3, 9]], ...}
+        # aim: create a matrix where each row is an atom and each column is a hyperedge (cliques + edges)
+        edges = [[edge[0], edge[1]] for edge in G.edges]
         cliques = self.get_functional_groups(mol)
-        num_hyperedges = len(cliques)
+        hyperedges = edges + list(cliques.values())
+        num_hyperedges = len(hyperedges)
+        incidence_hyperedges = torch.zeros(data.num_nodes, num_hyperedges)
         # create incidence matrix for hyperedges
-        incidence_1 = torch.zeros(data.num_nodes, num_hyperedges)
-        for i, group in enumerate(cliques.values()):
-            for hyperedge in group:
-                for atom in hyperedge:
-                    incidence_1[atom, i] = 1
+        for i, edge in enumerate(edges):
+            for atom in edge:
+                incidence_hyperedges[atom, i] = 1
+
+        # set functional groups attributes
+        fg_attr = self.get_functional_groups_attributes(mol, cliques)
+        ccc.set_cell_attributes(fg_attr)
 
         # Create the lifted topology dict for the cell complex
         ccc_lifted_topology = Graph2CellLifting._get_lifted_topology(self, ccc, G)
@@ -240,7 +377,7 @@ class CombinatorialFunctionalLifting(Graph2CombinatorialLifting):
         ccc_lifted_topology["num_hyperedges"] = num_hyperedges
         ccc_lifted_topology["x_0"] = data.x
         ccc_lifted_topology["incidence_hyperedges"] = torch.Tensor(
-            incidence_1
+            incidence_hyperedges
         ).to_sparse_coo()
 
         return ccc_lifted_topology
