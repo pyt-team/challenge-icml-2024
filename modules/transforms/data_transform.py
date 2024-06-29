59eaca5e0031dffbade654850ecffd4e230825e5
Theo Long <theodorelong98@gmail.com>
2024-06-29 17:35:37 -0400

    add alpha complex

--- modules/transforms/data_transform.py
+++ modules/transforms/data_transform.py
@@ -15,6 +15,9 @@ from modules.transforms.liftings.graph2hypergraph.knn_lifting import (
 from modules.transforms.liftings.graph2simplicial.clique_lifting import (
     SimplicialCliqueLifting,
 )
+from modules.transforms.liftings.pointcloud2simplicial.alpha_complex_lifting import (
+    AlphaComplexLifting,
+)
 
 TRANSFORMS = {
     # Graph -> Hypergraph
@@ -23,6 +26,8 @@ TRANSFORMS = {
     "SimplicialCliqueLifting": SimplicialCliqueLifting,
     # Graph -> Cell Complex
     "CellCycleLifting": CellCycleLifting,
+    # Point Cloud -> Simplicial Complex,
+    "AlphaComplexLifting": AlphaComplexLifting,
     # Feature Liftings
     "ProjectionSum": ProjectionSum,
     # Data Manipulations
--- modules/transforms/liftings/pointcloud2simplicial/base.py
+++ modules/transforms/liftings/pointcloud2simplicial/base.py
@@ -1,3 +1,6 @@
+from toponetx.classes import SimplicialComplex
+
+from modules.data.utils.utils import get_complex_connectivity
 from modules.transforms.liftings.lifting import PointCloudLifting
 
 
@@ -11,6 +14,21 @@ class PointCloud2SimplicialLifting(PointCloudLifting):
     **kwargs : optional
         Additional arguments for the class.
     """
+
+    def _get_lifted_topology(self, simplicial_complex: SimplicialComplex) -> dict:
+        r"""Returns the lifted topology.
+
+        Parameters
+        ----------
+        simplicial_complex : SimplicialComplex
+            The simplicial complex.
+        Returns
+        ---------
+        dict
+            The lifted topology.
+        """
+        lifted_topology = get_complex_connectivity(simplicial_complex, self.complex_dim)
+        return lifted_topology
 
     def __init__(self, complex_dim=2, **kwargs):
         super().__init__(**kwargs)
