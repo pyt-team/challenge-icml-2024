c4c31f786591bf30beda705655165ea9280e1a41
Bertran Miquel Oliver <bertranmiqueloliver@gmail.com>
2024-07-01 17:44:47 +0200

    Adding attributes

--- modules/transforms/liftings/graph2combinatorial/ring_close_atoms_lifting.py
+++ modules/transforms/liftings/graph2combinatorial/ring_close_atoms_lifting.py
@@ -2,6 +2,7 @@ import networkx as nx
 import torch
 import torch_geometric
 from rdkit import Chem
+from rdkit.Chem import Descriptors
 from toponetx.classes import CellComplex
 
 from modules.transforms.liftings.graph2cell.base import Graph2CellLifting
@@ -69,12 +70,14 @@ class CombinatorialRingCloseAtomsLifting(Graph2CombinatorialLifting):
         Chem.Mol
             The molecule.
         """
-        mol = Chem.MolFromSmiles(data.smiles)
-        return Chem.rdmolops.RemoveHs(mol)
+        # mol = Chem.MolFromSmiles(data.smiles)
+        # return Chem.rdmolops.RemoveHs(mol)
+        return Chem.MolFromSmiles(data.smiles)
 
     #######################################################
     ################ CLOSE ATOMS ##########################
     #######################################################
+
     def get_distance_matrix(
         self, data: torch_geometric.data.Data | dict
     ) -> torch.Tensor:
@@ -90,13 +93,18 @@ class CombinatorialRingCloseAtomsLifting(Graph2CombinatorialLifting):
         torch.Tensor
             The pairwise distance matrix.
         """
-        # Get the positions of the atoms
+        # Get the positions of the atoms different from hydrogen
         pos = data.pos
+        # keep only the non-hydrogen atoms
+        mask = data.x[:, 5] != 1
+        pos = pos[mask]
 
         # Compute the pairwise distances between the atoms
         return torch.cdist(pos, pos, p=2)
 
-    def find_close_atoms(self, data: torch_geometric.data.Data | dict) -> list:
+    def find_close_atoms(
+        self, data: torch_geometric.data.Data | dict
+    ) -> list:
         r"""Finds the atoms that are close to each other within a molecule.
 
         Parameters
@@ -109,7 +117,8 @@ class CombinatorialRingCloseAtomsLifting(Graph2CombinatorialLifting):
         list
             The list of close atoms.
         """
-        # Get the distances between atoms including in transormed data
+        # Get the distances between atoms excluding hydrogen atoms
+        # data = self._remove_hydrogens(data)
         distance_matrix = self.get_distance_matrix(data)
 
         # Get indices of atom pairs that are closer than the threshold
@@ -121,7 +130,9 @@ class CombinatorialRingCloseAtomsLifting(Graph2CombinatorialLifting):
             if distance_matrix[i, j] < float(self.threshold_distance)
         ]
 
-    def find_close_atom_groups(self, data: torch_geometric.data.Data | dict) -> list:
+    def find_close_atom_groups(
+            self, data: torch_geometric.data.Data | dict
+        ) -> list:
         r"""Finds the groups of atoms that are close to each other within a molecule.
 
         Parameters
@@ -139,6 +150,215 @@ class CombinatorialRingCloseAtomsLifting(Graph2CombinatorialLifting):
         G = nx.Graph()
         G.add_edges_from(close_atoms)
         return [list(component) for component in nx.connected_components(G)]
+
+    #######################################################
+    ################### ATTRIBUTES ########################
+    #######################################################
+
+    def get_atom_attributes(
+            self, mol: Chem.Mol
+        ) -> dict:
+        r"""Returns the atom attributes for each molecule.
+
+        Parameters
+        ----------
+        mol : Chem.Mol
+            The molecule.
+
+        Returns
+        -------
+        dict
+            The atom attributes with the atom id.
+            Example:
+            {
+                0: {"atomic_num": 6, "symbol": "C", "degree": 3, "formal_charge": 0, "hybridization": "SP
+                    "is_aromatic": False, "mass": 12.01, "chirality": "CHI_UNSPECIFIED"},
+
+                1: {"atomic_num": 6, "symbol": "C", "degree": 2, "formal_charge": 0, "hybridization": "SP
+                    "is_aromatic": False, "mass": 12.01, "chirality": "CHI_UNSPECIFIED"},
+                ...
+            }
+        """
+        attr = {}
+        for atom in mol.GetAtoms():
+            attr[atom.GetIdx()] = {
+                    "atomic_num": atom.GetAtomicNum(),
+                    "symbol": atom.GetSymbol(),
+                    "degree": atom.GetDegree(),
+                    "formal_charge": atom.GetFormalCharge(),
+                    "hybridization": str(atom.GetHybridization()),
+                    "is_aromatic": atom.GetIsAromatic(),
+                    "mass": atom.GetMass(),
+                    "chirality": atom.GetChiralTag()
+                }
+        return attr
+
+    def get_bond_attributes(
+            self, mol: Chem.Mol
+        ) -> torch.Tensor:
+        r"""Returns the bond attributes for each molecule.
+
+        Parameters
+        ----------
+        mol : Chem.Mol
+            The molecule.
+
+        Returns
+        -------
+        dict
+            The bond attributes with the bond id.
+            Example:
+            {
+                (0, 1): {"bond_type": "SINGLE", "is_conjugated": False, "is_stereo": False},
+                (1, 2): {"bond_type": "DOUBLE", "is_conjugated": False, "is_stereo": False},
+                ...
+            }
+        """
+        attr = {}
+        for bond in mol.GetBonds():
+            attr[(bond.GetBeginAtomIdx(), bond.GetEndAtomIdx())] = {
+                        "bond_type": str(bond.GetBondType()),
+                        "is_conjugated": bond.GetIsConjugated(),
+                        "is_stereo": bond.GetStereo()
+                    }
+        return attr
+
+    def get_ring_attributes(
+            self, mol: Chem.Mol, rings: list
+        ) -> dict:
+        r"""Returns the ring attributes for each molecule.
+
+        • Ring Size: Number of atoms that form the ring.
+        • Aromaticity: A binary feature telling whether the ring is aromatic.
+        • Has Heteroatom: A binary feature telling whether the ring contains a heteroatom,
+          which is an atom other than carbon, such as nitrogen, oxygen, or sulfur.
+        • Saturation: A binary feature telling whether the ring is saturated, meaning all
+          the bonds between carbon atoms are single bonds.
+        • Hydrophobicity: Tendency of the functional group to repel water.
+             Compute based in logp.
+        • Electrophilicity: Tendency of the functional group to accept electrons during a chemical reaction.
+             Compute based on the number of hydrogen bonds acceptors.
+        • Nucleophilicity: Tendency of the functional group to donate electrons during a chemical reaction.
+             High nucleophilicity means the functional group readily donates electrons, affecting its reactivity
+             with electrophiles. Compute based in number of hydrogen bond donors.
+        • Polarity: Distribution of electron density within the functional group. High polarity can significantly
+             affect solubility, reactivity, and interactions with other molecules.
+             Compute based on the tpsa.
+        Parameters
+        ----------
+        mol : Chem.Mol
+            The molecule.
+
+        rings : list
+            The rings of the molecule.
+            Example: [[0, 1, 2], [1, 2, 3], ...]
+
+        Returns
+        -------
+        dict
+            The ring attributes.
+            Example:
+            {
+                (0, 1, 2): {"ring_size": 3, "aromaticity": False, "has_heteroatom": False,
+                            "saturation": True, "hydrophobicity": 0.0, "electrophilicity": 0,
+                            "nucleophilicity": 0, "polarity": 0.0},
+                (1, 2, 3): {"ring_size": 3, "aromaticity": False, "has_heteroatom": False,
+                            "saturation": True, "hydrophobicity": 0.0, "electrophilicity": 0,
+                            "nucleophilicity": 0, "polarity": 0.0},
+                ...
+            }
+        """
+        attr = {}
+        for ring in rings:
+            ring_size = len(ring)
+            mol_ring = Chem.MolFromSmiles("".join([mol.GetAtomWithIdx(atom).GetSymbol() for atom in ring]))
+            aromaticity = all([atom.GetIsAromatic() for atom in mol_ring.GetAtoms()])
+            has_heteroatom = any([atom.GetAtomicNum() != 6 for atom in mol_ring.GetAtoms()])
+            saturation = all([bond.GetBondType() == Chem.rdchem.BondType.SINGLE for bond in mol_ring.GetBonds()])
+
+            hydrophobicity = Descriptors.MolLogP(mol_ring)
+            electrophilicity = Descriptors.NumHAcceptors(mol_ring)
+            nucleophilicity = Descriptors.NumHDonors(mol_ring)
+            polarity = Descriptors.TPSA(mol_ring)
+
+            attr[tuple(ring)] = {
+                "ring_size": ring_size,
+                "aromaticity": aromaticity,
+                "has_heteroatom": has_heteroatom,
+                "saturation": saturation,
+                "hydrophobicity": hydrophobicity,
+                "electrophilicity": electrophilicity,
+                "nucleophilicity": nucleophilicity,
+                "polarity": polarity
+            }
+
+        return attr
+
+
+    def get_close_atoms_attributes(
+            self, mol: Chem.Mol, close_atoms: list
+        ) -> dict:
+        r"""Returns the functional groups attributes for each molecule.
+
+        • Conjugation: A binary feature telling whether the functional group
+            is part of a conjugated system, which involves alternating double and single bonds.
+        • Hydrophobicity: Tendency of the functional group to repel water.
+             Compute based in logp.
+        • Electrophilicity: Tendency of the functional group to accept electrons during a chemical reaction.
+             Compute based on the number of hydrogen bonds acceptors.
+        • Nucleophilicity: Tendency of the functional group to donate electrons during a chemical reaction.
+             High nucleophilicity means the functional group readily donates electrons, affecting its reactivity
+             with electrophiles. Compute based in number of hydrogen bond donors.
+        • Polarity: Distribution of electron density within the functional group. High polarity can significantly
+             affect solubility, reactivity, and interactions with other molecules.
+             Compute based on the tpsa.
+
+        Parameters
+        ----------
+        mol : Chem.Mol
+            The molecule.
+
+        close_atoms : list
+            The close atoms groups of the molecule.
+            Example:
+            [[0, 1], [1, 2, 4], ...]
+
+        Returns
+        -------
+        dict
+            The functional groups attributes.
+            Example:
+            {
+                (0, 1): {"close_atoms_group": "C(=O)", "num_atoms": 2, "conjugation": False,
+                            "hydrophobicity": 0.0, "electrophilicity": 0, "nucleophilicity": 0, "polarity": 0.0},
+                (1, 2, 4): {"close_atoms_group": "C(=O)O", "num_atoms": 3, "conjugation": False,
+                            "hydrophobicity": 0.0, "electrophilicity": 0, "nucleophilicity": 0, "polarity": 0.0},
+                ...
+            }
+        """
+
+        attr = {}
+        for group in close_atoms:
+            fg_mol = Chem.MolFromSmiles("".join([mol.GetAtomWithIdx(atom).GetSymbol() for atom in group]))
+            Chem.SanitizeMol(fg_mol)
+
+            conjugation = all([bond.GetIsConjugated() for bond in fg_mol.GetBonds()])
+            hydrophobicity = Descriptors.MolLogP(fg_mol)
+            electrophilicity = Descriptors.NumHAcceptors(fg_mol)
+            nucleophilicity = Descriptors.NumHDonors(fg_mol)
+            polarity = Descriptors.TPSA(fg_mol)
+
+            attr[tuple(group)] = {
+                "functional_group": fg_mol,
+                "num_atoms": len(group),
+                "conjugation": conjugation,
+                "hydrophobicity": hydrophobicity,
+                "electrophilicity": electrophilicity,
+                "nucleophilicity": nucleophilicity,
+                "polarity": polarity
+            }
+
+        return attr
 
     #######################################################
     ################### LIFT ##############################
@@ -175,6 +395,20 @@ class CombinatorialRingCloseAtomsLifting(Graph2CombinatorialLifting):
         for ring in rings:
             ccc.add_cell(ring, rank=self.complex_dim)
 
+        mol = self._generate_mol_from_data(data)
+
+        # add atom attributes
+        atom_attributes = self.get_atom_attributes(mol)
+        ccc.set_cell_attributes(atom_attributes, rank=0)
+
+        # add bond attributes
+        bond_attributes = self.get_bond_attributes(mol)
+        ccc.set_cell_attributes(bond_attributes, rank=1)
+
+        # add ring attributes
+        ring_attributes = self.get_ring_attributes(mol, rings)
+        ccc.set_cell_attributes(ring_attributes, rank=2)
+
         # Hypergraph stuff
         # add close atoms as hyperedges (rank = 1)
         close_atoms = self.find_close_atom_groups(data)
@@ -184,6 +418,10 @@ class CombinatorialRingCloseAtomsLifting(Graph2CombinatorialLifting):
         for i, hyperedge in enumerate(close_atoms):
             for atom in hyperedge:
                 incidence_1[atom, i] = 1
+
+        # add hyperedge attributes
+        close_atoms_attributes = self.get_close_atoms_attributes(mol, close_atoms)
+        ccc.set_cell_attributes(close_atoms_attributes, rank=2) # if rank = 1, error in the code when close atoms are of more than 2 atoms
 
         # Create the lifted topology dict for the cell complex
         ccc_lifted_topology = Graph2CellLifting._get_lifted_topology(self, ccc, G)
