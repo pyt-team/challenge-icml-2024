b82bf771dc4ba759b7938bcc892fb4740101745a
Snopoff <snopoffp@gmail.com>
2024-07-06 14:06:45 +0300

    added ipynb; Jul 6 commit before future fixes

--- modules/transforms/liftings/pointcloud2simplicial/witness_lifting.py
+++ modules/transforms/liftings/pointcloud2simplicial/witness_lifting.py
@@ -1,4 +1,5 @@
 import gudhi as gd
+import torch
 import torch_geometric
 
 from modules.data.utils.utils import get_complex_connectivity
@@ -8,10 +9,29 @@ from modules.transforms.liftings.pointcloud2simplicial.base import (
 
 
 class WitnessLifting(PointCloud2SimplicialLifting):
-    def __init__(self, is_weat=True, is_euclidian=True, **kwargs):
+    def __init__(self, is_weak=True, is_euclidian=True, **kwargs):
         super().__init__(**kwargs)
-        self.is_weat = is_weat
-        self.is_euclidian = is_euclidian
+        self.is_weak = is_weak
+        self.is_euclidian = True  # is_euclidian
 
-    def lift_topology(self, data: torch_geometric.data.Data) -> dict:
-        pass
+    def lift_topology(
+        self,
+        witnesses: torch_geometric.data.Data,
+        landmarks: torch_geometric.data.Data = None,
+        landmark_proportion: int = 0.15,
+    ) -> dict:
+        n = len(witnesses.pos)
+        if not landmarks:
+            perm = torch.randperm(n)
+            idx = perm[: round(n * landmark_proportion)]
+            landmarks_position = witnesses.pos[idx]
+            print(landmarks_position)
+        else:
+            landmarks_position = landmarks.pos
+
+        if self.is_euclidian:
+            if self.is_weak:
+                complex = gd.EuclideanWitnessComplex(witnesses.pos, landmarks_position)
+                pass
+            else:
+                pass
