46d79cee11f686a8d0c71a4821532972646aa71e
Theo Long <theodorelong98@gmail.com>
2024-07-06 19:41:33 -0400

    add testing

--- test/transforms/liftings/pointcloud2simplicial/test_feature_rips_complex_lifting.py
+++ test/transforms/liftings/pointcloud2simplicial/test_feature_rips_complex_lifting.py
@@ -12,54 +12,176 @@ class TestFeatureRipsComplexLifting:
     """Test the FeatureRipsComplexLifting class."""
 
     def setup_method(self):
-        # Load the graph
+        # Load the point cloud
         self.data = load_manual_points()
 
-        # Initialise the SimplicialCliqueLifting class
-        self.lifting = FeatureRipsComplexLifting(complex_dim=3, alpha=25.0)
+        # Initialise the FeatureRipsLifting class
+        self.position_lifting = FeatureRipsComplexLifting(
+            complex_dim=3, feature_percent=0.0, max_edge_length=10.0
+        )
+        self.feature_lifting = FeatureRipsComplexLifting(
+            complex_dim=3, feature_percent=1.0, max_edge_length=1.0
+        )
+        self.mixed_lifting = FeatureRipsComplexLifting(
+            complex_dim=3, feature_percent=0.2, max_edge_length=10.0
+        )
 
     def test_lift_topology(self):
         """Test the lift_topology method."""
 
         # Test the lift_topology method
-        lifted_data = self.lifting.forward(self.data.clone())
+        lifted_data = self.position_lifting.forward(self.data.clone())
 
-        expected_incidence_1 = torch.tensor(
-            [
-                [1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-                [1.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-                [0.0, 1.0, 1.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0],
-                [0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0],
-                [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0],
-                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0],
-                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0],
-            ]
+        expected_incidences = (
+            torch.tensor(
+                [
+                    [1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                    [1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                    [0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0],
+                    [0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0],
+                    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0],
+                ]
+            ),
+            torch.tensor(
+                [
+                    [1.0, 1.0, 0.0, 0.0, 0.0],
+                    [1.0, 0.0, 1.0, 0.0, 0.0],
+                    [0.0, 1.0, 1.0, 0.0, 0.0],
+                    [1.0, 0.0, 0.0, 1.0, 0.0],
+                    [0.0, 1.0, 0.0, 1.0, 0.0],
+                    [0.0, 0.0, 1.0, 1.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 1.0],
+                    [0.0, 0.0, 0.0, 0.0, 1.0],
+                    [0.0, 0.0, 0.0, 0.0, 1.0],
+                ]
+            ),
+            torch.tensor([[1.0], [1.0], [1.0], [1.0], [0.0]]),
         )
 
         assert (
-            expected_incidence_1 == lifted_data.incidence_1.to_dense()
-        ).all(), "Something is wrong with incidence_1 (nodes to edges)."
+            expected_incidences[0] == lifted_data.incidence_1.to_dense()
+        ).all(), "Something is wrong with incidence_1 (nodes to edges) for feature_percent=0.0."
 
-        expected_incidence_2 = torch.tensor(
-            [
-                [1.0, 0.0, 0.0],
-                [1.0, 0.0, 0.0],
-                [1.0, 1.0, 0.0],
-                [0.0, 1.0, 0.0],
-                [0.0, 1.0, 0.0],
-                [0.0, 0.0, 0.0],
-                [0.0, 0.0, 1.0],
-                [0.0, 0.0, 1.0],
-                [0.0, 0.0, 1.0],
-            ]
+        assert (
+            abs(expected_incidences[1]) == lifted_data.incidence_2.to_dense()
+        ).all(), "Something is wrong with incidence_2 (edges to triangles) for feature_percent=0.0."
+
+        assert (
+            abs(expected_incidences[2]) == lifted_data.incidence_3.to_dense()
+        ).all(), "Something is wrong with incidence_3 (triangles to tetrahedrons) for feature_percent=0.0."
+
+        expected_incidences = (
+            torch.tensor(
+                [
+                    [1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                    [1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                    [0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                    [0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0],
+                    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0],
+                ]
+            ),
+            torch.tensor(
+                [
+                    [1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                    [1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                    [0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                    [1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+                    [0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+                    [0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0],
+                    [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0],
+                    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0],
+                ]
+            ),
+            torch.tensor(
+                [
+                    [1.0, 0.0],
+                    [1.0, 0.0],
+                    [1.0, 0.0],
+                    [1.0, 0.0],
+                    [0.0, 1.0],
+                    [0.0, 1.0],
+                    [0.0, 1.0],
+                    [0.0, 1.0],
+                ]
+            ),
+        )
+        lifted_data = self.feature_lifting.forward(self.data.clone())
+
+        assert (
+            expected_incidences[0] == lifted_data.incidence_1.to_dense()
+        ).all(), "Something is wrong with incidence_1 (nodes to edges) for feature_percent=1.0."
+
+        assert (
+            abs(expected_incidences[1]) == lifted_data.incidence_2.to_dense()
+        ).all(), "Something is wrong with incidence_2 (edges to triangles) for feature_percent=1.0."
+
+        assert (
+            abs(expected_incidences[2]) == lifted_data.incidence_3.to_dense()
+        ).all(), "Something is wrong with incidence_3 (triangles to tetrahedrons) for feature_percent=1.0."
+
+        expected_incidences = (
+            torch.tensor(
+                [
+                    [1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                    [1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                    [0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                    [0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0],
+                    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 1.0],
+                ]
+            ),
+            torch.tensor(
+                [
+                    [1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                    [1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                    [0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                    [1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                    [0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                    [0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+                    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0],
+                    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+                ]
+            ),
+            torch.tensor(
+                [
+                    [1.0, 0.0],
+                    [1.0, 0.0],
+                    [1.0, 0.0],
+                    [1.0, 0.0],
+                    [0.0, 1.0],
+                    [0.0, 1.0],
+                    [0.0, 1.0],
+                    [0.0, 1.0],
+                    [0.0, 0.0],
+                ]
+            ),
         )
 
-        assert (
-            abs(expected_incidence_2) == lifted_data.incidence_2.to_dense()
-        ).all(), "Something is wrong with incidence_2 (edges to triangles)."
-
-        expected_incidence_3 = torch.tensor([])
+        lifted_data = self.mixed_lifting.forward(self.data.clone())
 
         assert (
-            abs(expected_incidence_3) == lifted_data.incidence_3.to_dense()
-        ).all(), "Something is wrong with incidence_3 (triangles to tetrahedrons)."
+            expected_incidences[0] == lifted_data.incidence_1.to_dense()
+        ).all(), "Something is wrong with incidence_1 (nodes to edges) for feature_percent=0.2 ."
+
+        assert (
+            abs(expected_incidences[1]) == lifted_data.incidence_2.to_dense()
+        ).all(), "Something is wrong with incidence_2 (edges to triangles) for feature_percent=0.2 ."
+
+        assert (
+            abs(expected_incidences[2]) == lifted_data.incidence_3.to_dense()
+        ).all(), "Something is wrong with incidence_3 (triangles to tetrahedrons) for feature_percent=0.2 ."
