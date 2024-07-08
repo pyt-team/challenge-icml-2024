d6fe5ef5862f2b9e122225a5c07ea7c90dc05c27
demi <demi@MacBook-Pro-2.local>
2024-07-08 16:17:47 -0400

    update first pass on attribute lifting

---
+++ modules/transforms/liftings/graph2hypergraph/attribute_lifting.py
@@ -0,0 +1,50 @@
+import warnings
+
+import networkx
+import torch
+import torch_geometric
+
+from modules.transforms.liftings.graph2hypergraph.base import Graph2HypergraphLifting
+
+
+class NodeAttributeLifting(Graph2HypergraphLifting):
+    r"""Lifts graphs to hypergraph domain by grouping nodes with the same attribute.
+
+    Parameters
+    ----------
+    attribute_idx : int
+        The index of the node attribute to use for hyperedge construction.
+    """
+
+    def __init__(self, attribute_idx: int, **kwargs):
+        super().__init__(**kwargs)
+        self.attribute_idx = attribute_idx
+
+    def lift_topology(self, data: torch_geometric.data.Data) -> dict:
+        r"""Lifts the topology of a graph to hypergraph domain by grouping nodes with the same attribute.
+
+        Parameters
+        ----------
+        data : torch_geometric.data.Data
+            The input data to be lifted.
+
+        Returns
+        -------
+        dict
+            The lifted topology.
+        """
+        attribute = data.x[:, self.attribute_idx]
+        unique_attributes = torch.unique(attribute)
+        num_hyperedges = unique_attributes.size(0)
+
+        incidence_1 = torch.zeros(data.num_nodes, num_hyperedges)
+        for i, attr in enumerate(unique_attributes):
+            nodes_with_attr = torch.where(attribute == attr)[0]
+            incidence_1[nodes_with_attr, i] = 1
+
+        incidence_1 = incidence_1.to_sparse_coo()
+        return {
+            "incidence_hyperedges": incidence_1,
+            "num_hyperedges": num_hyperedges,
+            "x_0": data.x,
+        }
---
+++ test/transforms/liftings/graph2hypergraph/test_attribute_lifting.py
@@ -0,0 +1,49 @@
+import torch
+import torch_geometric
+
+from modules.data.utils.utils import load_manual_graph
+from modules.transforms.liftings.graph2hypergraph.attribute_lifting import (
+    NodeAttributeLifting,
+)
+
+
+class TestNodeAttributeLifting:
+    """Test the NodeAttributeLifting class."""
+
+    def setup_method(self):
+        # Set up a simple manual graph for testing
+        self.data = torch_geometric.data.Data(
+            x=torch.tensor(
+                [
+                    [1, 0],
+                    [1, 0],
+                    [0, 1],
+                    [0, 1],
+                ],
+                dtype=torch.float,
+            ),
+            edge_index=torch.tensor([[0, 1, 2, 3], [1, 0, 3, 2]], dtype=torch.long),
+        )
+        self.lifting = NodeAttributeLifting(attribute_idx=0)
+
+    def test_lift_topology(self):
+        # Test the lift_topology method
+        lifted_topology = self.lifting.lift_topology(self.data.clone())
+
+        expected_n_hyperedges = 2
+        expected_incidence_1 = torch.tensor(
+            [
+                [1.0, 0.0],
+                [1.0, 0.0],
+                [0.0, 1.0],
+                [0.0, 1.0],
+            ]
+        ).to_sparse_coo()
+
+        assert torch.equal(
+            expected_incidence_1.to_dense(),
+            lifted_topology["incidence_hyperedges"].to_dense(),
+        ), "Something is wrong with incidence_hyperedges."
+        assert (
+            expected_n_hyperedges == lifted_topology["num_hyperedges"]
+        ), "Something is wrong with the number of hyperedges."
