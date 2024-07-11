9d00a1c67e891ebbe9e766a3ea62d3f28c4b20ce
Snopoff <snopoffp@gmail.com>
2024-07-11 23:55:55 +0300

    implemented core

--- modules/transforms/liftings/pointcloud2simplicial/witness_lifting.py
+++ modules/transforms/liftings/pointcloud2simplicial/witness_lifting.py
@@ -1,6 +1,7 @@
 import gudhi as gd
 import torch
 import torch_geometric
+from toponetx.classes import SimplicialComplex
 
 from modules.data.utils.utils import get_complex_connectivity
 from modules.transforms.liftings.pointcloud2simplicial.base import (
@@ -9,29 +10,63 @@ from modules.transforms.liftings.pointcloud2simplicial.base import (
 
 
 class WitnessLifting(PointCloud2SimplicialLifting):
-    def __init__(self, is_weak=True, is_euclidian=True, **kwargs):
+    def __init__(
+        self,
+        is_weak=True,
+        is_euclidian=True,
+        landmark_proportion: int = 1,
+        max_alpha_square=0.1,
+        complex_dim=2,
+        **kwargs,
+    ):
         super().__init__(**kwargs)
         self.is_weak = is_weak
-        self.is_euclidian = True  # is_euclidian
+        self.is_euclidian = is_euclidian
+        self.landmark_proportion = landmark_proportion
+        self.max_alpha_square = max_alpha_square
+        self.complex_dim = complex_dim
+
+    def _get_lifted_topology(self, simplicial_complex: SimplicialComplex) -> dict:
+        r"""Returns the lifted topology.
+        Parameters
+        ----------
+        simplicial_complex : SimplicialComplex
+            The simplicial complex.
+        Returns
+        ---------
+        dict
+            The lifted topology.
+        """
+        lifted_topology = get_complex_connectivity(simplicial_complex, self.complex_dim)
+        lifted_topology["x_0"] = torch.stack(
+            list(simplicial_complex.get_simplex_attributes("features", 0).values())
+        )
+
+        return lifted_topology
 
     def lift_topology(
         self,
         witnesses: torch_geometric.data.Data,
-        landmarks: torch_geometric.data.Data = None,
-        landmark_proportion: int = 0.15,
     ) -> dict:
         n = len(witnesses.pos)
-        if not landmarks:
-            perm = torch.randperm(n)
-            idx = perm[: round(n * landmark_proportion)]
-            landmarks_position = witnesses.pos[idx]
-            print(landmarks_position)
-        else:
-            landmarks_position = landmarks.pos
+        perm = torch.randperm(n)
+        idx = perm[: round(n * self.landmark_proportion)]
+        landmarks_position = witnesses.pos[idx]
 
         if self.is_euclidian:
             if self.is_weak:
                 complex = gd.EuclideanWitnessComplex(witnesses.pos, landmarks_position)
-                pass
+                simplex_tree = complex.create_simplex_tree(
+                    self.max_alpha_square, self.complex_dim
+                )
+                dimension = simplex_tree.dimension()
+                filtered_skeleta = simplex_tree.get_skeleton(dimension)
+                skeleta = list(map(lambda x: x[0], filtered_skeleta))
+                simplicial_complex = SimplicialComplex(skeleta)
             else:
                 pass
+
+        node_features = {i: witnesses.x[i, :] for i in range(witnesses.x.shape[0])}
+        simplicial_complex.set_simplex_attributes(node_features, name="features")
+
+        return self._get_lifted_topology(simplicial_complex)
