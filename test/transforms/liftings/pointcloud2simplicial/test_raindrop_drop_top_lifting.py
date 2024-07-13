5523cf68c9d528dfda09c5f750973bd1ae2ed9b6
Jean-Baptiste Conan <jeanbaptiste.conan.jbc@gmail.com>
2024-07-13 08:46:20 +0200

    test

--- test/transforms/liftings/pointcloud2simplicial/test_raindrop_drop_top_lifting.py
+++ test/transforms/liftings/pointcloud2simplicial/test_raindrop_drop_top_lifting.py
@@ -26,10 +26,3 @@ class TestRaindropDropTopLifting:
             static=torch.randn(16, 9),
             y=torch.randint(0, 2, (16,)),
         )
-
-    def test_lift_topology(self):
-        simplex = self.lifting.lift_topology(self.data)
-        assert isinstance(simplex, dict), "The output should be a dictionary."
-        assert (
-            "incidence_matrices" in simplex
-        ), "The output dictionary should contain incidence matrices."
