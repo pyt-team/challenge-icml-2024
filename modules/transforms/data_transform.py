c0a4b901debec3ba61cca69d240bad5e0eb1859f
Alessandro Salatiello <salatiello.alessandro@gmail.com>
2024-07-13 13:49:50 +0200

    Added spectral lifting

--- modules/transforms/data_transform.py
+++ modules/transforms/data_transform.py
@@ -12,6 +12,9 @@ from modules.transforms.liftings.graph2cell.cycle_lifting import CellCycleLiftin
 from modules.transforms.liftings.graph2hypergraph.knn_lifting import (
     HypergraphKNNLifting,
 )
+from modules.transforms.liftings.graph2hypergraph.spectral_lifting import (
+    SpectralLifting,
+)
 from modules.transforms.liftings.graph2simplicial.clique_lifting import (
     SimplicialCliqueLifting,
 )
@@ -19,6 +22,7 @@ from modules.transforms.liftings.graph2simplicial.clique_lifting import (
 TRANSFORMS = {
     # Graph -> Hypergraph
     "HypergraphKNNLifting": HypergraphKNNLifting,
+    "SpectralLifting": SpectralLifting,
     # Graph -> Simplicial Complex
     "SimplicialCliqueLifting": SimplicialCliqueLifting,
     # Graph -> Cell Complex
---
+++ test/transforms/liftings/graph2hypergraph/test_spectral_lifting.py
@@ -0,0 +1,29 @@
+"""Test the message passing module."""
+
+import torch
+
+from modules.data.utils.utils import load_manual_graph
+from modules.transforms.liftings.graph2hypergraph.spectral_lifting import (
+    SpectralLifting,
+)
+
+
+class TestSpectralLifting:
+    """Test the SpectralLifting class."""
+
+    def setup_method(self):
+        # Load the graph
+        self.data = load_manual_graph()
+
+        # Initialise the SpectralLifting class
+        self.num_hyperedges = 3
+        self.lifting = SpectralLifting(n_c=self.num_hyperedges)
+
+    def test_lift_topology(self):
+        # Test the lift_topology method
+        lifted_data_k = self.lifting.forward(self.data.clone())
+
+        assert list(lifted_data_k.incidence_hyperedges.shape) == [
+            self.data.num_nodes,
+            self.num_hyperedges,
+        ], "There were issues computing the incidence_hyperedges matrix"
