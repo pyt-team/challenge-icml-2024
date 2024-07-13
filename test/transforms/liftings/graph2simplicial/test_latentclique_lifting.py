0fe19fd648cc74c30829cdd66513726531625de5
mauricio <mauriciogtec@gmail.com>
2024-07-13 03:41:21 +0000

    update test to comply with Black/PEP8

--- test/transforms/liftings/graph2simplicial/test_latentclique_lifting.py
+++ test/transforms/liftings/graph2simplicial/test_latentclique_lifting.py
@@ -1,19 +1,19 @@
 """Test the message passing module."""
 
+import random
+
+import networkx as nx
 import torch
 from torch_geometric.data import Data
 
 from modules.data.utils.utils import load_manual_graph
-from modules.transforms.liftings.graph2simplicial.clique_lifting import (
-    SimplicialCliqueLifting,
-)
+from modules.transforms.liftings.graph2simplicial.clique_lifting import \
+    SimplicialCliqueLifting
 from modules.transforms.liftings.graph2simplicial.latentclique_lifting import (
-    LatentCliqueLifting, _sample_from_ibp
-)
-import random
-import networkx as nx
+    LatentCliqueLifting, _sample_from_ibp)
 
-def create_clique_graph(num_nodes = 8):
+
+def create_clique_graph(num_nodes=8):
     """
     Create a torch_geometric.data.Data object representing a single clique graph with node features being all ones.
 
@@ -40,6 +40,7 @@ def create_clique_graph(num_nodes = 8):
 
     return data
 
+
 class TestLatentCliqueCoverLifting:
     """Test the LatentCliqueCoverLifting class."""
 
@@ -48,23 +49,29 @@ class TestLatentCliqueCoverLifting:
         self.data_test_one = create_clique_graph()
         self.data_test_two = load_manual_graph()
         # Initialise the SimplicialCliqueLifting class
-        self.lifting_edge_prob_one = LatentCliqueLifting(edge_prob_mean =1)
-        self.lifting_edge_prob_any = LatentCliqueLifting(edge_prob = random.uniform(0, 1))
+        self.lifting_edge_prob_one = LatentCliqueLifting(edge_prob_mean=1)
+        self.lifting_edge_prob_any = LatentCliqueLifting(edge_prob=random.uniform(0, 1))
 
     def test_lift_topology(self):
         """Test the lift_topology method."""
 
         # Test the lift_topology method
-        edge_prob_one_adj = self.lifting_edge_prob_one.forward(self.data_test_one.clone()).adjacency_0
-        edge_prob_any_adj = self.lifting_edge_prob_any.forward(self.data_test_two.clone()).adjacency_0
+        edge_prob_one_adj = self.lifting_edge_prob_one.forward(
+            self.data_test_one.clone()
+        ).adjacency_0
+        edge_prob_any_adj = self.lifting_edge_prob_any.forward(
+            self.data_test_two.clone()
+        ).adjacency_0
 
         ### TEST #1 ###
         # if edge_prob == 1 and a the input graph has a single clique,
-        # then the 1-skeleton of the inferred latent SC must have a single clique 
+        # then the 1-skeleton of the inferred latent SC must have a single clique
         # (or, equivalently, the SC has a simplex in its facests set if complex_dim = |maximal_clique|-1)
 
         # Convert adjacency matrix to NetworkX graph
-        G_from_latent_complex = nx.from_numpy_matrix(edge_prob_one_adj.to_dense().numpy())
+        G_from_latent_complex = nx.from_numpy_matrix(
+            edge_prob_one_adj.to_dense().numpy()
+        )
         G_input = nx.Graph()
         G_input.add_edges_from(self.data_test_one.edge_index.t().tolist())
 
@@ -76,23 +83,28 @@ class TestLatentCliqueCoverLifting:
         num_cliques_latent = len(cliques_latent)
         num_cliques_input = len(cliques_input)
 
-        assert num_cliques_latent == num_cliques_latent , ("the 1-skeleton of the inferred latent SC does not have a single clique")
+        assert (
+            num_cliques_latent == num_cliques_latent
+        ), "the 1-skeleton of the inferred latent SC does not have a single clique"
 
-        ### TEST #2 ### 
+        ### TEST #2 ###
         # if edge_prob in (0,1] the set of 0-simplices (edges) of the inferred
         # latent SC must be a superset of the set of edges of the input graph
         # (or, equivalently, there is no subset of the 1-skeleton of the SC isomorphic to the input graph)
 
         # Convert adjacency matrix to NetworkX graph
-        G_from_latent_complex = nx.from_numpy_matrix(edge_prob_any_adj.to_dense().numpy())
+        G_from_latent_complex = nx.from_numpy_matrix(
+            edge_prob_any_adj.to_dense().numpy()
+        )
         G_input = nx.Graph()
         G_input.add_edges_from(self.data_test_two.edge_index.t().tolist())
 
         # Edges are Undirected
-        latent_edge_set = {tuple(sorted((edge))) for edge in G_from_latent_complex.edges} 
-        input_edge_set = {tuple(sorted((edge))) for edge in G_input.edges} 
-        
-        assert input_edge_set.issubset(latent_edge_set), ("the set of 0-simplices of the inferred latent SC is not contained the set of edges of the input graph")
+        latent_edge_set = {
+            tuple(sorted((edge))) for edge in G_from_latent_complex.edges
+        }
+        input_edge_set = {tuple(sorted((edge))) for edge in G_input.edges}
 
-
-        
+        assert input_edge_set.issubset(
+            latent_edge_set
+        ), "the set of 0-simplices of the inferred latent SC is not contained the set of edges of the input graph"
