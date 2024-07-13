617993c8956f23363d078e01f6a7fd0e35e0feaf
Theo Long <theodorelong98@gmail.com>
2024-07-13 01:13:01 -0400

    add test

--- test/transforms/liftings/pointcloud2simplicial/test_feature_rips_complex_lifting.py
+++ test/transforms/liftings/pointcloud2simplicial/test_feature_rips_complex_lifting.py
@@ -1,6 +1,7 @@
 """Test the feature-based Rips complex lifting."""
 
 import torch
+import torch_geometric
 
 from modules.data.utils.utils import load_manual_points
 from modules.transforms.liftings.pointcloud2simplicial.feature_rips_complex_lifting import (
@@ -25,6 +26,27 @@ class TestFeatureRipsComplexLifting:
         self.mixed_lifting = FeatureRipsComplexLifting(
             complex_dim=3, feature_percent=0.2, max_edge_length=10.0
         )
+        self.no_epsilon_lifting = FeatureRipsComplexLifting(
+            complex_dim=3,
+            feature_percent=0.5,
+            max_edge_length=10.0,
+            epsilon=0.0,
+        )
+
+    def test_generate_distance_matrix(self):
+        # Test the generate_distance_matrix method
+        data = torch_geometric.data.Data(
+            pos=torch.tensor([[-1], [1], [2]]).float(),
+            x=torch.tensor([[2], [4], [6]]).float(),
+        )
+        expected_pairwise_distances = torch.tensor(
+            [[0, 2, 3.5], [2, 0, 1.5], [3.5, 1.5, 0]]
+        )
+
+        assert (
+            self.no_epsilon_lifting.generate_distance_matrix(data)
+            == expected_pairwise_distances
+        ).all(), "generate_distance_matrix not working as expected"
 
     def test_lift_topology(self):
         """Test the lift_topology method."""
